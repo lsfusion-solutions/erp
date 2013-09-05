ee0aeb19f886fe4ff8b8fe214b284efe7b332aa7
AFilipchyk <filipchyk.andrei@gmail.com>
2013-09-05 12:14:42 +0000

    [erp] ловим exception в barcodeUtils

--- erp-logics/src/main/java/lsfusion/erp/stock/BarcodeUtils.java
+++ erp-logics/src/main/java/lsfusion/erp/stock/BarcodeUtils.java
@@ -3,17 +3,21 @@ package lsfusion.erp.stock;
 public class BarcodeUtils {
 
     public static String convertBarcode12To13(String barcode) {
-        if (barcode != null && barcode.length() == 12) {
-            int checkSum = 0;
-            for (int i = 0; i <= 10; i = i + 2) {
-                checkSum += Integer.valueOf(String.valueOf(barcode.charAt(i)));
-                checkSum += Integer.valueOf(String.valueOf(barcode.charAt(i + 1))) * 3;
-            }
-            checkSum %= 10;
-            if (checkSum != 0)
-                checkSum = 10 - checkSum;
-            return barcode.concat(String.valueOf(checkSum));
-        } else
+        try {
+            if (barcode != null && barcode.length() == 12) {
+                int checkSum = 0;
+                for (int i = 0; i <= 10; i = i + 2) {
+                    checkSum += Integer.valueOf(String.valueOf(barcode.charAt(i)));
+                    checkSum += Integer.valueOf(String.valueOf(barcode.charAt(i + 1))) * 3;
+                }
+                checkSum %= 10;
+                if (checkSum != 0)
+                    checkSum = 10 - checkSum;
+                return barcode.concat(String.valueOf(checkSum));
+            } else
+                return barcode;
+        } catch (Exception e) {
             return barcode;
+        }
     }
 }
