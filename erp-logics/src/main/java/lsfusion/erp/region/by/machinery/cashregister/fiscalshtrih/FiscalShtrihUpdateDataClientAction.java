81f7b4ac9442e693d6964d52330cd7936aa6818b
AFilipchyk <filipchyk.andrei@gmail.com>
2013-10-15 07:46:43 +0000

    [erp] shtrih: добавляем Thread.sleep(1000) после каждой операции записи в таблицу

--- erp-logics/src/main/java/lsfusion/erp/region/by/machinery/cashregister/fiscalshtrih/FiscalShtrihUpdateDataClientAction.java
+++ erp-logics/src/main/java/lsfusion/erp/region/by/machinery/cashregister/fiscalshtrih/FiscalShtrihUpdateDataClientAction.java
@@ -4,6 +4,7 @@ import lsfusion.interop.action.ClientAction;
 import lsfusion.interop.action.ClientActionDispatcher;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 
 
 public class FiscalShtrihUpdateDataClientAction implements ClientAction {
@@ -16,7 +17,7 @@ public class FiscalShtrihUpdateDataClientAction implements ClientAction {
     public FiscalShtrihUpdateDataClientAction(int password, Integer comPort, Integer baudRate, UpdateDataInstance updateData) {
         this.password = password;
         this.comPort = comPort == null ? 0 : comPort;
-        this.baudRate = baudRate== null ? 0 : baudRate;
+        this.baudRate = baudRate == null ? 0 : baudRate;
         this.updateData = updateData;
     }
 
@@ -27,22 +28,26 @@ public class FiscalShtrihUpdateDataClientAction implements ClientAction {
         try {
 
             FiscalShtrih.openPort(password, comPort, baudRate);
-
-            FiscalShtrih.resetTable(2);
-            if(updateData.operatorList.isEmpty())
-                updateData.operatorList.add(new UpdateDataOperator(1000, 1, "Кассир по умолчанию"));
-            for (int i = 1; i <= updateData.operatorList.size(); i++) {
-                FiscalShtrih.setOperatorName(updateData.operatorList.get(i-1));
+            //FiscalShtrih.resetTable(2);
+            //if (updateData.operatorList.isEmpty())
+            //    updateData.operatorList.add(new UpdateDataOperator(1000, 1, "Кассир по умолчанию"));
+            for (int i = 1; i <= 28/*updateData.operatorList.size()*/; i++) {
+                FiscalShtrih.setOperatorName(updateData.operatorList.size() < (i - 1) ? new UpdateDataOperator(i * 1000, i, "Кассир по умолчанию") : updateData.operatorList.get(i - 1));
+                Thread.sleep(1000);
             }
 
-            FiscalShtrih.resetTable(6);
-            for (int i = 1; i <= Math.min(updateData.taxRateList.size(), 4); i++) {
-                FiscalShtrih.setTaxRate(updateData.taxRateList.get(i-1));
-            }            
-            
+            //FiscalShtrih.resetTable(6);
+            for (int i = 1; i <= /*Math.min(updateData.taxRateList.size(),*/ 4/*)*/; i++) {
+                FiscalShtrih.setTaxRate(updateData.taxRateList.size() < (i - 1) ? new UpdateDataTaxRate(i, BigDecimal.ZERO) : updateData.taxRateList.get(i - 1));
+                Thread.sleep(1000);
+            }
+
             FiscalShtrih.closePort();
 
         } catch (RuntimeException e) {
+            FiscalShtrih.closePort();
+            return e.getMessage();
+        } catch (InterruptedException e) {
             FiscalShtrih.closePort();
             return e.getMessage();
         }
