ae29aa51d213c3f11706ad3a2cf6d8e7bc60efa4
AFilipchyk <filipchyk.andrei@gmail.com>
2014-01-17 11:45:00 +0000

    [erp] мониторинг активных блокировок postgresql

--- erp-logics/src/main/java/lsfusion/erp/utils/GetActiveBlocksPostgreSQLActionProperty.java
+++ erp-logics/src/main/java/lsfusion/erp/utils/GetActiveBlocksPostgreSQLActionProperty.java
@@ -9,8 +9,8 @@ import lsfusion.base.col.interfaces.immutable.ImOrderMap;
 import lsfusion.base.col.interfaces.immutable.ImSet;
 import lsfusion.base.col.interfaces.mutable.MExclMap;
 import lsfusion.base.col.interfaces.mutable.MExclSet;
-import lsfusion.server.classes.DateTimeClass;
 import lsfusion.server.classes.IntegerClass;
+import lsfusion.server.classes.LogicalClass;
 import lsfusion.server.classes.StringClass;
 import lsfusion.server.data.SQLHandledException;
 import lsfusion.server.data.query.ExecuteEnvironment;
@@ -26,11 +26,10 @@ import lsfusion.server.logics.scripted.ScriptingLogicsModule;
 import lsfusion.server.session.DataSession;
 
 import java.sql.SQLException;
-import java.sql.Timestamp;
 
-public class GetActiveTasksPostgreSQLActionProperty extends ScriptingActionProperty {
+public class GetActiveBlocksPostgreSQLActionProperty extends ScriptingActionProperty {
 
-    public GetActiveTasksPostgreSQLActionProperty(ScriptingLogicsModule LM) {
+    public GetActiveBlocksPostgreSQLActionProperty(ScriptingLogicsModule LM) {
         super(LM);
     }
 
@@ -39,7 +38,7 @@ public class GetActiveTasksPostgreSQLActionProperty extends ScriptingActionPrope
 
         try {
 
-            getActiveTasksFromDatabase(context);
+            getActiveBlocksFromDatabase(context);
 
         } catch (SQLHandledException e) {
             throw Throwables.propagate(e);
@@ -50,23 +49,23 @@ public class GetActiveTasksPostgreSQLActionProperty extends ScriptingActionPrope
     }
 
 
-    private void getActiveTasksFromDatabase(ExecutionContext context) throws SQLException, SQLHandledException, ScriptingErrorLog.SemanticErrorException {
+    private void getActiveBlocksFromDatabase(ExecutionContext context) throws SQLException, SQLHandledException, ScriptingErrorLog.SemanticErrorException {
 
         DataSession session = context.getSession();
 
-        Integer previousCount = (Integer) getLCP("previousCountActiveTask").read(session);
+        Integer previousCount = (Integer) getLCP("previousCountActiveBlock").read(session);
         previousCount = previousCount == null ? 0 : previousCount;
 
         for (int i = 0; i < previousCount; i++) {
             DataObject currentObject = new DataObject(i);
-            getLCP("idActiveTask").change((Object) null, session, currentObject);
-            getLCP("queryActiveTask").change((Object) null, session, currentObject);
-            getLCP("userActiveTask").change((Object) null, session, currentObject);
-            getLCP("addressUserActiveTask").change((Object) null, session, currentObject);
-            getLCP("dateTimeActiveTask").change((Object) null, session, currentObject);
+            getLCP("typeLockedObjectActiveBlock").change((Object) null, session, currentObject);
+            getLCP("idLockedObjectActiveBlock").change((Object) null, session, currentObject);
+            getLCP("processIdActiveBlock").change((Object) null, session, currentObject);
+            getLCP("modeActiveBlock").change((Object) null, session, currentObject);
+            getLCP("grantedActiveBlock").change((Object) null, session, currentObject);
         }
 
-        String originalQuery = String.format("SELECT * FROM pg_stat_activity WHERE datname='%s' AND state!='idle'", context.getBL().getDataBaseName());
+        String originalQuery = "SELECT relation::regclass, * FROM pg_locks";  
 
         MExclSet<String> keyNames = SetFact.mExclSet();
         keyNames.exclAdd("numberrow");
@@ -77,19 +76,23 @@ public class GetActiveTasksPostgreSQLActionProperty extends ScriptingActionPrope
         keyReaders.immutable();
 
         MExclSet<String> propertyNames = SetFact.mExclSet();
-        propertyNames.exclAdd("query");
+        propertyNames.exclAdd("transactionid");
+        propertyNames.exclAdd("virtualxid");
+        propertyNames.exclAdd("relation");
+        propertyNames.exclAdd("locktype");
         propertyNames.exclAdd("pid");
-        propertyNames.exclAdd("usename");
-        propertyNames.exclAdd("client_addr");
-        propertyNames.exclAdd("query_start");
+        propertyNames.exclAdd("mode");
+        propertyNames.exclAdd("granted");
         propertyNames.immutable();
 
         MExclMap<String, Reader> propertyReaders = MapFact.mExclMap();
-        propertyReaders.exclAdd("query", StringClass.get(1000));
+        propertyReaders.exclAdd("transactionid", PGObjectReader.instance);
+        propertyReaders.exclAdd("virtualxid", StringClass.get(100));
+        propertyReaders.exclAdd("relation", PGObjectReader.instance);
+        propertyReaders.exclAdd("locktype", StringClass.get(100));
         propertyReaders.exclAdd("pid", IntegerClass.instance);
-        propertyReaders.exclAdd("usename", StringClass.get(100));
-        propertyReaders.exclAdd("client_addr", PGObjectReader.instance);
-        propertyReaders.exclAdd("query_start", DateTimeClass.instance);
+        propertyReaders.exclAdd("mode", StringClass.get(100));
+        propertyReaders.exclAdd("granted", LogicalClass.instance);
         propertyReaders.immutable();
 
         ImOrderMap rs = session.sql.executeSelect(originalQuery, ExecuteEnvironment.EMPTY, (ImMap<String, ParseInterface>) MapFact.mExclMap(), 
@@ -100,24 +103,33 @@ public class GetActiveTasksPostgreSQLActionProperty extends ScriptingActionPrope
 
             HMap entry = (HMap) rsValue;
 
+
             DataObject currentObject = new DataObject(i);
 
-            String query = trim((String) entry.get("query"));
+            String transactionID = trim((String) entry.get("transactionid"));
+            String virtualxID = trim((String) entry.get("virtualxid"));
+            String relationID = trim((String) entry.get("relation"));
+            String type = trim((String) entry.get("locktype"));
+            String mode = trim((String) entry.get("mode"));
+            String lockedType = trim((String) entry.get("locktype"));
             Integer processId = (Integer) entry.get("pid");
-            String userActiveTask = trim((String) entry.get("usename"));
-            String address = trim((String) entry.get("client_addr"));
-            Timestamp dateTime = (Timestamp) entry.get("query_start");
-            if (!query.equals(originalQuery)) {
+            Boolean granted = (Boolean) entry.get("granted");
+            
+            if (type != null && type.equals("transactionid"))
+                getLCP("idLockedObjectActiveBlock").change(transactionID, session, currentObject);
+            else if (type != null && type.equals("virtualxid"))
+                getLCP("idLockedObjectActiveBlock").change(virtualxID, session, currentObject);
+            else if (type != null && type.equals("relation"))
+                getLCP("idLockedObjectActiveBlock").change(relationID, session, currentObject);
 
-                getLCP("idActiveTask").change(processId, session, currentObject);
-                getLCP("queryActiveTask").change(query, session, currentObject);
-                getLCP("userActiveTask").change(userActiveTask, session, currentObject);
-                getLCP("addressUserActiveTask").change(address, session, currentObject);
-                getLCP("dateTimeActiveTask").change(dateTime, session, currentObject);
-                i++;
-            }
+            getLCP("processIdActiveBlock").change(processId, session, currentObject);
+            getLCP("modeActiveBlock").change(mode, session, currentObject);
+            getLCP("typeLockedObjectActiveBlock").change(lockedType, session, currentObject);
+            getLCP("grantedActiveBlock").change(granted, session, currentObject);
+            
+            i++;
         }
-        getLCP("previousCountActiveTask").change(i, session);
+        getLCP("previousCountActiveBlock").change(i, session);
     }
 
     protected String trim(String input) {
