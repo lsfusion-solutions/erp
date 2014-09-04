acc03a50a37acab803fcdd3a7f97a01b74961163
AFilipchyk <filipchyk.andrei@gmail.com>
2014-09-04 08:58:20 +0000

    [erp, foodbox] выгрузка в CSV и одна форма foodbox для примера

--- erp-logics/src/main/java/lsfusion/erp/utils/ExportCSVActionProperty.java
+++ erp-logics/src/main/java/lsfusion/erp/utils/ExportCSVActionProperty.java
@@ -28,7 +28,7 @@ public abstract class ExportCSVActionProperty extends ScriptingActionProperty {
         this.idGroupObject = idGroupObject;
     }
 
-    public void executeCustom(ExecutionContext<ClassPropertyInterface> context, Map<String, DataObject> valuesMap) throws SQLException, SQLHandledException {
+    public void executeCustom(ExecutionContext<ClassPropertyInterface> context, Map<String, DataObject> valuesMap, String filePath, String separator) throws SQLException, SQLHandledException {
 
         try {
 
@@ -41,25 +41,25 @@ public abstract class ExportCSVActionProperty extends ScriptingActionProperty {
                 if (valuesMap != null)
                     for (Map.Entry<String, DataObject> entry : valuesMap.entrySet())
                         formInstance.forceChangeObject(formInstance.instanceFactory.getInstance(LM.getObjectEntityByName(formEntity, entry.getKey())), entry.getValue());
-
-                String filePath = "xxx";
-                String separator = ";";
                 
                 File exportFile = new File(filePath);
-                PrintWriter bw = new PrintWriter(exportFile, "cp1251");
-                
-                String titleString = "";
-                for(Object property : formInstance.getCalcProperties())
-                    titleString +=((PropertyDrawInstance) property).propertyObject.property.caption + separator;
-                titleString = titleString.isEmpty() ? titleString : titleString.substring(0, titleString.length() - separator.length());
-                bw.println(titleString);
+                PrintWriter bw = new PrintWriter(exportFile, "cp1251");                             
 
                 FormData formData = formInstance.getFormData(0);
 
+                boolean firstLine = true;
                 for(FormRow row : formData.rows) {
+                    if(firstLine) {
+                        String titleString = "";
+                        for(Object property : row.values.keys())
+                            titleString +=((PropertyDrawInstance) property).propertyObject.property.caption + separator;
+                        titleString = titleString.isEmpty() ? titleString : titleString.substring(0, titleString.length() - separator.length());
+                        bw.println(titleString);
+                        firstLine = false;
+                    }
                     String rowString = "";
                     for(Object property : row.values.values()) 
-                        rowString += property + separator;
+                        rowString += (property == null ? "" : property) + separator;
                     rowString = rowString.isEmpty() ? rowString : rowString.substring(0, rowString.length() - separator.length());
                     bw.println(rowString);
                 }
