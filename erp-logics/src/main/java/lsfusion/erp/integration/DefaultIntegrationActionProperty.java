05c7cbffba0286225395cf4ae3347af3ab9ad243
AFilipchyk <filipchyk.andrei@gmail.com>
2014-09-10 10:59:50 +0000

    [green] поправки импорта

--- erp-logics/src/main/java/lsfusion/erp/integration/DefaultIntegrationActionProperty.java
+++ erp-logics/src/main/java/lsfusion/erp/integration/DefaultIntegrationActionProperty.java
@@ -96,6 +96,10 @@ public class DefaultIntegrationActionProperty extends ScriptingActionProperty {
         else return (operand1 == null ? operand2 : (operand2 == null ? operand1 : operand1.add(operand2)));
     }
 
+    protected BigDecimal safeSubtract(int operand1, BigDecimal operand2) {
+        return safeSubtract(BigDecimal.valueOf(operand1), operand2);
+    }
+    
     protected BigDecimal safeSubtract(BigDecimal operand1, int operand2) {
         return safeSubtract(operand1, BigDecimal.valueOf(operand2));
     }
