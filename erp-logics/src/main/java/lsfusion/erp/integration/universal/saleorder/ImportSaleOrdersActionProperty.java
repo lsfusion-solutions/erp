89a8c5cc0141e6750a2e26b1ad1b19c54d6d2a91
AFilipchyk <filipchyk.andrei@gmail.com>
2014-12-22 09:04:00 +0000

    [erp] importSaleOrders fix

--- erp-logics/src/main/java/lsfusion/erp/integration/universal/saleorder/ImportSaleOrdersActionProperty.java
+++ erp-logics/src/main/java/lsfusion/erp/integration/universal/saleorder/ImportSaleOrdersActionProperty.java
@@ -84,7 +84,7 @@ public class ImportSaleOrdersActionProperty extends ImportDocumentActionProperty
 
                                 try {
 
-                                    boolean importResult = new ImportSaleOrderActionProperty(LM).makeImport(context.getBL(),
+                                    boolean importResult = new ImportSaleOrderActionProperty(LM, findClass("Sale.UserOrder")).makeImport(context.getBL(),
                                             currentSession, orderObject, importColumns, IOUtils.getFileBytes(f), settings,
                                             fileExtension, operationObject, supplierObject, supplierStockObject, customerObject,
                                             customerStockObject);                                                                                                        
