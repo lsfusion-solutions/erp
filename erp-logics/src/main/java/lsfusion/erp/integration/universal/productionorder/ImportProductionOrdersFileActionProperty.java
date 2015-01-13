0bf05a056b4a74a30c32bf7a24630d665c9d63ab
AFilipchyk <filipchyk.andrei@gmail.com>
2015-01-13 12:24:34 +0000

    [vitalur] Импортировать заказы из файла

---
+++ erp-logics/src/main/java/lsfusion/erp/integration/universal/productionorder/ImportProductionOrdersFileActionProperty.java
@@ -0,0 +1,83 @@
+package lsfusion.erp.integration.universal.productionorder;
+
+import jxl.read.biff.BiffException;
+import lsfusion.erp.integration.universal.ImportColumnDetail;
+import lsfusion.erp.integration.universal.ImportDocumentActionProperty;
+import lsfusion.erp.integration.universal.ImportDocumentSettings;
+import lsfusion.erp.integration.universal.UniversalImportException;
+import lsfusion.interop.action.MessageClientAction;
+import lsfusion.server.classes.CustomStaticFormatFileClass;
+import lsfusion.server.data.SQLHandledException;
+import lsfusion.server.logics.DataObject;
+import lsfusion.server.logics.NullValue;
+import lsfusion.server.logics.ObjectValue;
+import lsfusion.server.logics.property.ClassPropertyInterface;
+import lsfusion.server.logics.property.ExecutionContext;
+import lsfusion.server.logics.scripted.ScriptingErrorLog;
+import lsfusion.server.logics.scripted.ScriptingLogicsModule;
+import lsfusion.server.session.DataSession;
+import org.xBaseJ.xBaseJException;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.List;
+import java.util.Map;
+
+public class ImportProductionOrdersFileActionProperty extends ImportDocumentActionProperty {
+    
+    public ImportProductionOrdersFileActionProperty(ScriptingLogicsModule LM) throws ScriptingErrorLog.SemanticErrorException {
+        super(LM);
+    }
+
+    @Override
+    public void executeCustom(ExecutionContext<ClassPropertyInterface> context) throws SQLException, SQLHandledException {
+
+        try {
+
+            DataSession session = context.getSession();
+
+            ObjectValue importTypeObject = findProperty("importTypeOrders").readClasses(session);
+
+            if (!(importTypeObject instanceof NullValue)) {
+
+                ObjectValue operationObject = findProperty("autoImportOperationImportType").readClasses(session, (DataObject) importTypeObject);
+
+                Map<String, ImportColumnDetail> importColumns = readImportColumns(session, importTypeObject).get(0);
+                ImportDocumentSettings settings = readImportDocumentSettings(session, importTypeObject);
+                String fileExtension = settings.getFileExtension();
+
+                if (importColumns != null && fileExtension != null) {
+
+                    CustomStaticFormatFileClass valueClass = CustomStaticFormatFileClass.get(false, false, fileExtension + " Files", fileExtension);
+                    ObjectValue objectValue = context.requestUserData(valueClass, null);
+                    if (objectValue != null) {
+                        List<byte[]> fileList = valueClass.getFiles(objectValue.getValue());
+
+                        for (byte[] file : fileList) {
+
+                            new ImportProductionOrderActionProperty(LM).makeImport(context.getBL(), session, null, importColumns, file, settings, fileExtension, operationObject);
+
+                            session.apply(context);
+                            
+                            findAction("formRefresh").execute(context);
+                        }
+                    }
+                }
+            }
+        } catch (ScriptingErrorLog.SemanticErrorException e) {
+            throw new RuntimeException(e);
+        } catch (xBaseJException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (BiffException e) {
+            throw new RuntimeException(e);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        } catch (UniversalImportException e) {
+            e.printStackTrace();
+            context.requestUserInteraction(new MessageClientAction(e.getMessage(), e.getTitle()));
+        }
+    }
+}
