6dd415d5d44eeb9229afca7bebf4b9b1dcf52baf
CrushBy <crushby@gmail.com>
2015-07-09 11:26:23 +0000

    Исправлен прием скидок для SET5

--- equ-clt/src/main/java/equ/clt/handler/kristal/KristalHandler.java
+++ equ-clt/src/main/java/equ/clt/handler/kristal/KristalHandler.java
@@ -904,7 +904,7 @@ public class KristalHandler extends CashRegisterHandler<KristalSalesBatch> {
                                                         currentSalesInfoList.add(new SalesInfo(false, directoryGroupCashRegisterMap.get(directory + "_" + numberCashRegister),
                                                                 numberCashRegister, numberZReport, numberReceipt, dateReceipt, timeReceipt, idEmployee, null, null, sumCard,
                                                                 sumCash, null, barcode, idItem, null, quantity, price, sumReceiptDetail, discountSumReceiptDetail,
-                                                                discountSumReceipt, discountCard, numberReceiptDetail, fileName));
+                                                                null, discountCard, numberReceiptDetail, fileName));
                                                 }
 
                                                 //чит для случая, когда не указана сумма платежа. Недостающую сумму пишем в наличные.
