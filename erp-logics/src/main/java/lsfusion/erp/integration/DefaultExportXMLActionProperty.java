337d2ec8b6614d0e4e945098229518ecd9f25ab3
AFilipchyk <filipchyk.andrei@gmail.com>
2016-04-18 09:47:15 +0000

    [vitalur] Экспорт в XML (refs #3506)

---
+++ erp-logics/src/main/java/lsfusion/erp/integration/DefaultExportXMLActionProperty.java
@@ -0,0 +1,54 @@
+package lsfusion.erp.integration;
+
+import lsfusion.server.classes.ValueClass;
+import lsfusion.server.logics.scripted.ScriptingLogicsModule;
+import org.jdom.Attribute;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.Namespace;
+import org.jdom.output.Format;
+import org.jdom.output.XMLOutputter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.math.BigDecimal;
+
+public class DefaultExportXMLActionProperty extends DefaultExportActionProperty {
+
+    public DefaultExportXMLActionProperty(ScriptingLogicsModule LM) {
+        super(LM);
+    }
+
+    public DefaultExportXMLActionProperty(ScriptingLogicsModule LM, ValueClass... classes) {
+        super(LM, classes);
+    }
+
+    protected void outputXml(Document doc, Writer outputStreamWriter, String encoding) throws IOException {
+        XMLOutputter xmlOutput = new XMLOutputter();
+        xmlOutput.setFormat(Format.getPrettyFormat().setEncoding(encoding));
+        PrintWriter fw = new PrintWriter(outputStreamWriter);
+        xmlOutput.output(doc, fw);
+        fw.close();
+    }
+
+    protected void setAttribute(Element element, String id, Object value) {
+        if (value != null)
+            element.setAttribute(new Attribute(id, String.valueOf(value)));
+    }
+
+    protected void setAttribute(Element element, String id, Object value, Namespace namespace) {
+        if (value != null)
+            element.setAttribute(id, String.valueOf(value), namespace);
+    }
+
+    protected void addBigDecimalElement(Element parent, String id, BigDecimal value) {
+        if (value != null)
+            parent.addContent(new Element(id).setText(String.valueOf(value)));
+    }
+
+    protected void addStringElement(Element parent, String id, String value) {
+        if (value != null)
+            parent.addContent(new Element(id).setText(value));
+    }
+}
