fb1fa26f46ca63ae38313b61a227b831b5ab2e5b
AFilipchyk <filipchyk.andrei@gmail.com>
2016-04-25 12:25:39 +0000

    [erp] импорт декларанта: поддержка нескольких номеров деклараций в одном файле (с предоставлением выбора пользователю)

--- erp-logics/src/main/java/lsfusion/erp/region/by/certificate/declaration/ImportDeclarationDBFActionProperty.java
+++ erp-logics/src/main/java/lsfusion/erp/region/by/certificate/declaration/ImportDeclarationDBFActionProperty.java
@@ -8,6 +8,7 @@ import lsfusion.base.col.interfaces.immutable.ImOrderMap;
 import lsfusion.base.col.interfaces.immutable.ImRevMap;
 import lsfusion.erp.integration.DefaultImportDBFActionProperty;
 import lsfusion.interop.Compare;
+import lsfusion.interop.action.ChooseObjectClientAction;
 import lsfusion.interop.action.MessageClientAction;
 import lsfusion.server.classes.ConcreteCustomClass;
 import lsfusion.server.classes.CustomStaticFormatFileClass;
@@ -30,10 +31,7 @@ import java.io.File;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class ImportDeclarationDBFActionProperty extends DefaultImportDBFActionProperty {
     String charset = "cp866";
@@ -58,7 +56,7 @@ public class ImportDeclarationDBFActionProperty extends DefaultImportDBFActionPr
             if (objectValue != null) {
 
                 List<byte[]> fileList = valueClass.getFiles(objectValue.getValue());
-                
+
                 for (byte[] entry : fileList) {
 
                     importDeclaration(context, declarationObject, entry);
@@ -72,7 +70,7 @@ public class ImportDeclarationDBFActionProperty extends DefaultImportDBFActionPr
 
     private void importDeclaration(ExecutionContext context, DataObject declarationObject, byte[] entry) throws SQLException, ScriptingErrorLog.SemanticErrorException, IOException, xBaseJException, SQLHandledException {
 
-        List<List<Object>> data = readDeclarationFromDBF(context, declarationObject, entry);
+        Map<String, List<List<Object>>> declarationsMap = readDeclarationsFromDBF(context, declarationObject, entry);
 
         KeyExpr declarationDetailExpr = new KeyExpr("DeclarationDetail");
         ImRevMap<Object, KeyExpr> declarationDetailKeys = MapFact.singletonRev((Object) "declarationDetail", declarationDetailExpr);
@@ -81,10 +79,9 @@ public class ImportDeclarationDBFActionProperty extends DefaultImportDBFActionPr
         query.and(findProperty("declaration[DeclarationDetail]").getExpr(context.getModifier(), declarationDetailExpr).compare(declarationObject.getExpr(), Compare.EQUALS));
         ImOrderMap<ImMap<Object, Object>, ImMap<Object, Object>> result = query.execute(context);
 
-        if(result.size() != data.size())
-            context.requestUserInteraction(new MessageClientAction(String.format("Разное количество строк во входном файле G47 (%s) и в базе (%s)", data.size(), result.size()), "Ошибка"));
+        List<List<Object>> data = chooseDeclaration(context, declarationsMap, result.size());
 
-        else {
+        if (data != null) {
 
             List<ImportProperty<?>> props = new ArrayList<>();
             List<ImportField> fields = new ArrayList<>();
@@ -119,15 +116,15 @@ public class ImportDeclarationDBFActionProperty extends DefaultImportDBFActionPr
                 resultMessage = session.applyMessage(context);
                 session.popVolatileStats();
             }
-            if(resultMessage == null) {
+            if (resultMessage == null) {
                 context.requestUserInteraction(new MessageClientAction("Импорт успешно завершён", "Импорт из декларанта"));
             }
         }
     }
 
-    private List<List<Object>> readDeclarationFromDBF(ExecutionContext context, DataObject declarationObject, byte[] entry) throws ScriptingErrorLog.SemanticErrorException, SQLException, IOException, xBaseJException, SQLHandledException {
-        
-        List<List<Object>> data = new ArrayList<>();
+    private Map<String, List<List<Object>>> readDeclarationsFromDBF(ExecutionContext context, DataObject declarationObject, byte[] entry) throws ScriptingErrorLog.SemanticErrorException, SQLException, IOException, xBaseJException, SQLHandledException {
+
+        Map<String, List<List<Object>>> declarationsMap = new HashMap<>();
         File tempFile = null;
         DBF dbfFile = null;
         try {
@@ -139,51 +136,94 @@ public class ImportDeclarationDBFActionProperty extends DefaultImportDBFActionPr
 
             BigDecimal homeSum = null, dutySum = null, VATSum = null;
 
-            Integer curNumber = null;
+            String curNumberDeclaration = null;
+            Integer curNumberDeclarationDetail = null;
 
             for (int i = 0; i < recordCount; i++) {
 
                 dbfFile.read();
 
+                String numberDeclaration = getDBFFieldValue(dbfFile, "Nomer_GTD", charset);
+
                 Integer numberDeclarationDetail = getDBFIntegerFieldValue(dbfFile, "G32", charset);
 
-                if (curNumber != null && !curNumber.equals(numberDeclarationDetail)) {
-                    data.add(Arrays.asList((Object) curNumber, dutySum, VATSum, homeSum));
+                if ((curNumberDeclarationDetail != null && !curNumberDeclarationDetail.equals(numberDeclarationDetail)) || (curNumberDeclaration != null && !curNumberDeclaration.equals(numberDeclaration))) {
+                    List<List<Object>> declarationEntry = declarationsMap.get(curNumberDeclaration);
+                    if (declarationEntry == null)
+                        declarationEntry = new ArrayList<>();
+                    declarationEntry.add(Arrays.asList((Object) curNumberDeclarationDetail, dutySum, VATSum, homeSum));
+                    declarationsMap.put(curNumberDeclaration, declarationEntry);
                     dutySum = null;
                     VATSum = null;
                     homeSum = null;
                 }
-                curNumber = numberDeclarationDetail;
+                curNumberDeclaration = numberDeclaration;
+                curNumberDeclarationDetail = numberDeclarationDetail;
 
                 String g471 = trim(getDBFFieldValue(dbfFile, "G471", charset));
-
                 if (g471 != null) {
-                    if (g471.equals("2010")) {
-                        homeSum = getDBFBigDecimalFieldValue(dbfFile, "G472", charset);
-                        BigDecimal extraDutySum = getDBFBigDecimalFieldValue(dbfFile, "G474", charset);
-                        if (dutySum == null)
-                            dutySum = extraDutySum;
-                        else
-                            if (extraDutySum != null)
+                    switch (g471) {
+                        case "2010":
+                            homeSum = getDBFBigDecimalFieldValue(dbfFile, "G472", charset);
+                            BigDecimal extraDutySum = getDBFBigDecimalFieldValue(dbfFile, "G474", charset);
+                            if (dutySum == null)
+                                dutySum = extraDutySum;
+                            else if (extraDutySum != null)
                                 dutySum = dutySum.add(extraDutySum);
-                    } else if (g471.equals("5010")) {
-                        if (homeSum == null) homeSum = getDBFBigDecimalFieldValue(dbfFile, "G472", charset);
-                        VATSum = getDBFBigDecimalFieldValue(dbfFile, "G474", charset);
-                    } else if (g471.equals("1010")) {
-                        BigDecimal g474 = getDBFBigDecimalFieldValue(dbfFile, "G474", charset);  //dutySum - VATSum
-                        findProperty("registrationSum[Declaration]").change(g474, context, declarationObject);
+                            break;
+                        case "5010":
+                            if (homeSum == null) homeSum = getDBFBigDecimalFieldValue(dbfFile, "G472", charset);
+                            VATSum = getDBFBigDecimalFieldValue(dbfFile, "G474", charset);
+                            break;
+                        case "1010":
+                            BigDecimal g474 = getDBFBigDecimalFieldValue(dbfFile, "G474", charset);  //dutySum - VATSum
+
+                            findProperty("registrationSum[Declaration]").change(g474, context, declarationObject);
+                            break;
                     }
                 }
             }
 
-            if (curNumber != null) {
-                data.add(Arrays.asList((Object) curNumber, dutySum, VATSum, homeSum));
+            if (curNumberDeclarationDetail != null) {
+                List<List<Object>> declarationEntry = declarationsMap.get(curNumberDeclaration);
+                if (declarationEntry == null)
+                    declarationEntry = new ArrayList<>();
+                declarationEntry.add(Arrays.asList((Object) curNumberDeclarationDetail, dutySum, VATSum, homeSum));
+                declarationsMap.put(curNumberDeclaration, declarationEntry);
             }
         } finally {
-            if(dbfFile != null)
+            if (dbfFile != null)
                 dbfFile.close();
-            if(tempFile != null)
-                tempFile.delete();
+            if (tempFile != null && !tempFile.delete())
+                tempFile.deleteOnExit();
+        }
+        return declarationsMap;
+    }
+
+    private List<List<Object>> chooseDeclaration(ExecutionContext context, Map<String, List<List<Object>>> declarationMap, int size) {
+        List<List<Object>> data = null;
+        Object[][] variants = new Object[declarationMap.size()][1];
+        int i = 0;
+        for(String key : declarationMap.keySet()) {
+            variants[i][0] = key;
+            i++;
+        }
+        if (declarationMap.isEmpty())
+            context.requestUserInteraction(new MessageClientAction("Не найдено ни одной декларации во входном файле G47", "Ошибка"));
+        else {
+            Integer index = (Integer) (declarationMap.size() == 1 ? declarationMap.get(0) :
+                    context.requestUserInteraction(new ChooseObjectClientAction("Выберите декларацию", new String[]{"Номер декларации"}, variants)));
+            if(index != null) {
+                if (index == -1)
+                    index = 0;
+
+                data = declarationMap.get(variants[index][0]);
+                if (data.size() != size) {
+                    context.requestUserInteraction(new MessageClientAction(
+                            String.format("Разное количество строк во входном файле G47 (%s) и в базе (%s)", data.size(), size), "Ошибка"));
+                    data = null;
+                }
+            }
         }
         return data;
     }
