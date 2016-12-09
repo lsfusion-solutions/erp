a47354b3eccf6a0bf80c680310a7e2470afa7133
AFilipchyk <filipchyk.andrei@gmail.com>
2016-12-09 09:23:55 +0000

    [erp] Импорт KTC (XML)

---
+++ erp-logics/src/main/java/lsfusion/erp/region/by/certificate/declaration/ImportDeclarationAdjustmentXMLActionProperty.java
@@ -0,0 +1,131 @@
+package lsfusion.erp.region.by.certificate.declaration;
+
+import com.google.common.base.Throwables;
+import lsfusion.erp.integration.DefaultImportActionProperty;
+import lsfusion.interop.action.MessageClientAction;
+import lsfusion.server.classes.ConcreteCustomClass;
+import lsfusion.server.classes.CustomStaticFormatFileClass;
+import lsfusion.server.classes.ValueClass;
+import lsfusion.server.data.SQLHandledException;
+import lsfusion.server.integration.*;
+import lsfusion.server.logics.DataObject;
+import lsfusion.server.logics.ObjectValue;
+import lsfusion.server.logics.property.ClassPropertyInterface;
+import lsfusion.server.logics.property.ExecutionContext;
+import lsfusion.server.logics.scripted.ScriptingErrorLog;
+import lsfusion.server.logics.scripted.ScriptingLogicsModule;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.Namespace;
+import org.jdom.input.SAXBuilder;
+
+import java.io.ByteArrayInputStream;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+
+public class ImportDeclarationAdjustmentXMLActionProperty extends DefaultImportActionProperty {
+    private final ClassPropertyInterface declarationInterface;
+
+    public ImportDeclarationAdjustmentXMLActionProperty(ScriptingLogicsModule LM, ValueClass... classes) throws ScriptingErrorLog.SemanticErrorException {
+        super(LM, classes);
+
+        Iterator<ClassPropertyInterface> i = interfaces.iterator();
+        declarationInterface = i.next();
+    }
+
+    public void executeCustom(ExecutionContext<ClassPropertyInterface> context) throws SQLException, SQLHandledException {
+
+        try {
+
+            CustomStaticFormatFileClass valueClass = CustomStaticFormatFileClass.get(false, false, "Файлы XML", "xml");
+            ObjectValue objectValue = context.requestUserData(valueClass, null);
+            if (objectValue != null) {
+                List<byte[]> fileList = valueClass.getFiles(objectValue.getValue());
+
+                DataObject declarationObject = context.getDataKeyValue(declarationInterface);
+                for (byte[] file : fileList) {
+
+                    List<List<Object>> data = new ArrayList<>();
+
+                    SAXBuilder builder = new SAXBuilder();
+                    Document document = builder.build(new ByteArrayInputStream(file));
+                    Element rootNode = document.getRootElement();
+                    Namespace ns = rootNode.getNamespace("KDTout");
+                    Namespace gns = rootNode.getNamespace("catESAD_cu");
+                    Namespace catRu = rootNode.getNamespace("cat_ru");
+
+                    Element registryNumberNode = rootNode.getChild("GtdRegistryNumber", ns);
+                    Date registryDate = parseDate(registryNumberNode.getChildText("RegistrationDate", catRu));
+
+                    rootNode = rootNode.getChild("KDToutGoodsShipment", ns);
+
+                    List list = rootNode.getChildren("KDToutGoods", ns);
+                    for (Object aList : list) {
+                        Element node = (Element) aList;
+                        List payment = node.getChildren("KDToutCustomsPaymentCalculation", ns);
+
+                        Double duty = null;
+                        Double vat = null;
+                        for (Object p : payment) {
+                            String paymentModeCode = ((Element) p).getChildText("PaymentModeCode", gns);
+                            if ("2010".equals(paymentModeCode)) {
+                                duty = Double.valueOf(((Element) p).getChildText("PaymentAmount", gns));
+                            } else if ("5010".equals(paymentModeCode))
+                                vat = Double.valueOf(((Element) p).getChildText("PaymentAmount", gns));
+                        }
+                        Double sum = Double.valueOf(node.getChildText("CustomsCost", gns));
+                        Integer number = Integer.valueOf(node.getChildText("GoodsNumeric", gns));
+
+                        data.add(Arrays.<Object>asList(1, number, registryDate, duty, vat, sum));
+                    }
+
+                    List<ImportProperty<?>> properties = new ArrayList<>();
+                    List<ImportField> fields = new ArrayList<>();
+                    List<ImportKey<?>> keys = new ArrayList<>();
+
+                    ImportField numberDeclarationAdjustmentField = new ImportField(findProperty("number[DeclarationAdjustment]"));
+                    ImportKey<?> declarationAdjustmentKey = new ImportKey((ConcreteCustomClass) findClass("DeclarationAdjustment"),
+                            findProperty("declarationAdjustment[Declaration,INTEGER]").getMapping(declarationObject, numberDeclarationAdjustmentField));
+                    keys.add(declarationAdjustmentKey);
+                    properties.add(new ImportProperty(numberDeclarationAdjustmentField, findProperty("number[DeclarationAdjustment]").getMapping(declarationAdjustmentKey)));
+                    properties.add(new ImportProperty(declarationObject, findProperty("declaration[DeclarationAdjustment]").getMapping(declarationAdjustmentKey)));
+                    fields.add(numberDeclarationAdjustmentField);
+
+                    ImportField numberDeclarationDetailField = new ImportField(findProperty("number[DeclarationDetail]"));
+                    ImportKey<?> declarationDetailKey = new ImportKey((ConcreteCustomClass) findClass("DeclarationDetail"),
+                            findProperty("declarationDetail[Declaration,INTEGER]").getMapping(declarationObject, numberDeclarationDetailField));
+                    keys.add(declarationDetailKey);
+                    properties.add(new ImportProperty(declarationObject, findProperty("declaration[DeclarationDetail]").getMapping(declarationDetailKey)));
+                    fields.add(numberDeclarationDetailField);
+
+                    ImportField dateDeclarationAdjustment = new ImportField(findProperty("date[DeclarationAdjustment]"));
+                    properties.add(new ImportProperty(dateDeclarationAdjustment, findProperty("date[DeclarationAdjustment]").getMapping(declarationAdjustmentKey)));
+                    fields.add(dateDeclarationAdjustment);
+
+                    ImportField dutySumDeclarationAdjustmentField = new ImportField(findProperty("dutySum[DeclarationAdjustment, DeclarationDetail]"));
+                    properties.add(new ImportProperty(dutySumDeclarationAdjustmentField, findProperty("dutySum[DeclarationAdjustment, DeclarationDetail]").getMapping(declarationAdjustmentKey, declarationDetailKey)));
+                    fields.add(dutySumDeclarationAdjustmentField);
+
+                    ImportField VATSumDeclarationAdjustmentField = new ImportField(findProperty("VATSum[DeclarationAdjustment, DeclarationDetail]"));
+                    properties.add(new ImportProperty(VATSumDeclarationAdjustmentField, findProperty("VATSum[DeclarationAdjustment, DeclarationDetail]").getMapping(declarationAdjustmentKey, declarationDetailKey)));
+                    fields.add(VATSumDeclarationAdjustmentField);
+
+                    ImportField homeSumDeclarationAdjustmentField = new ImportField(findProperty("homeSum[DeclarationAdjustment, DeclarationDetail]"));
+                    properties.add(new ImportProperty(homeSumDeclarationAdjustmentField, findProperty("homeSum[DeclarationAdjustment, DeclarationDetail]").getMapping(declarationAdjustmentKey, declarationDetailKey)));
+                    fields.add(homeSumDeclarationAdjustmentField);
+
+                    IntegrationService integrationService = new IntegrationService(context.getSession(), new ImportTable(fields, data), keys, properties);
+                    integrationService.synchronize(true, false);
+                    context.requestUserInteraction(new MessageClientAction("Импорт успешно завершён", "Импорт КТС"));
+                }
+            }
+        } catch (Exception e) {
+            throw Throwables.propagate(e);
+        }
+    }
+}
--- erp-logics/src/main/java/lsfusion/erp/region/by/certificate/declaration/ImportXMLDeclarationActionProperty.java
+++ erp-logics/src/main/java/lsfusion/erp/region/by/certificate/declaration/ImportXMLDeclarationActionProperty.java
@@ -1,28 +1,28 @@
 package lsfusion.erp.region.by.certificate.declaration;
 
+import com.google.common.base.Throwables;
+import lsfusion.erp.integration.DefaultImportActionProperty;
+import lsfusion.server.classes.ConcreteCustomClass;
+import lsfusion.server.classes.CustomClass;
+import lsfusion.server.classes.CustomStaticFormatFileClass;
 import lsfusion.server.classes.ValueClass;
 import lsfusion.server.data.SQLHandledException;
-import org.jdom.Document;
-import org.jdom.Element;
-import org.jdom.JDOMException;
-import org.jdom.Namespace;
-import org.jdom.input.SAXBuilder;
-import lsfusion.base.BaseUtils;
-import lsfusion.server.classes.ConcreteCustomClass;
-import lsfusion.server.classes.CustomStaticFormatFileClass;
-import lsfusion.server.classes.DateClass;
 import lsfusion.server.integration.*;
 import lsfusion.server.logics.DataObject;
 import lsfusion.server.logics.ObjectValue;
 import lsfusion.server.logics.property.ClassPropertyInterface;
 import lsfusion.server.logics.property.ExecutionContext;
-import lsfusion.server.logics.scripted.ScriptingActionProperty;
 import lsfusion.server.logics.scripted.ScriptingErrorLog;
 import lsfusion.server.logics.scripted.ScriptingLogicsModule;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.Namespace;
+import org.jdom.input.SAXBuilder;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.sql.Date;
+import java.math.BigDecimal;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -30,9 +30,8 @@ import java.util.Iterator;
 import java.util.List;
 
 
-public class ImportXMLDeclarationActionProperty extends ScriptingActionProperty {
+public class ImportXMLDeclarationActionProperty extends DefaultImportActionProperty {
     private final ClassPropertyInterface declarationInterface;
-    List<Object> row;
 
     public ImportXMLDeclarationActionProperty(ScriptingLogicsModule LM, ValueClass... classes) throws ScriptingErrorLog.SemanticErrorException {
         super(LM, classes);
@@ -50,12 +49,10 @@ public class ImportXMLDeclarationActionProperty extends ScriptingActionProperty 
             if (objectValue != null) {
                 List<byte[]> fileList = valueClass.getFiles(objectValue.getValue());
 
-                DataObject declaration = context.getDataKeyValue(declarationInterface);
-                //ObjectValue customsZone = LM.findProperty("customsZoneDeclaration").readClasses(context.getSession(), declaration);
+                DataObject declarationObject = context.getDataKeyValue(declarationInterface);
                 for (byte[] file : fileList) {
 
                     List<List<Object>> data = new ArrayList<>();
-                    Date defaultDate = new Date(2011-1900, 0, 1);
 
                     SAXBuilder builder = new SAXBuilder();
                     Document document = builder.build(new ByteArrayInputStream(file));
@@ -65,21 +62,23 @@ public class ImportXMLDeclarationActionProperty extends ScriptingActionProperty 
                     Namespace cu = rootNode.getNamespace("cat_ru");
                     rootNode = rootNode.getChild("ESADout_CUGoodsShipment", ns);
                     List list = rootNode.getChildren("ESADout_CUGoods", ns);
-                    for (int i = 0; i < list.size(); i++) {
-                        Element node = (Element) list.get(i);
-                        row = new ArrayList<>();
+                    for (Object aList : list) {
+                        Element node = (Element) aList;
                         List payment = node.getChildren("ESADout_CUCustomsPaymentCalculation", ns);
 
-                        Double duty = null;
-                        Double vat = null;
+                        BigDecimal duty = null;
+                        BigDecimal sumVAT = null;
+                        BigDecimal valueVAT = null;
                         for (Object p : payment) {
                             String paymentModeCode = ((Element) p).getChildText("PaymentModeCode", gns);
                             if ("2010".equals(paymentModeCode)) {
-                                duty = Double.valueOf(((Element) p).getChildText("PaymentAmount", gns));
-                            } else if ("5010".equals(paymentModeCode))
-                                vat = Double.valueOf(((Element) p).getChildText("PaymentAmount", gns));
+                                duty = parseBigDecimal(((Element) p).getChildText("PaymentAmount", gns));
+                            } else if ("5010".equals(paymentModeCode)) {
+                                sumVAT = parseBigDecimal(((Element) p).getChildText("PaymentAmount", gns));
+                                valueVAT = parseBigDecimal(((Element) p).getChildText("Rate", gns));
+                            }
                         }
-                        Double sum = Double.valueOf(node.getChildText("CustomsCost", gns));
+                        BigDecimal sum = parseBigDecimal(node.getChildText("CustomsCost", gns));
                         Integer number = Integer.valueOf(node.getChildText("GoodsNumeric", gns));
                         String description = node.getChildText("GoodsDescription", gns);
                         String TNVED = node.getChildText("GoodsTNVEDCode", gns);
@@ -90,75 +89,91 @@ public class ImportXMLDeclarationActionProperty extends ScriptingActionProperty 
                         String uomName = goodsGroupQuantity.getChildText("MeasureUnitQualifierName", cu);
                         String uomCode = goodsGroupQuantity.getChildText("MeasureUnitQualifierCode", cu);
 
-                        row.add(number);
-                        row.add(description);
-                        row.add(sum);
-                        row.add(duty);
-                        row.add(vat);
-                        row.add(defaultDate);
-                        row.add(TNVED);
-                        row.add(countryCode);
-                        row.add(uomName);
-                        row.add(uomCode);
-                        data.add(row);
+                        data.add(Arrays.<Object>asList(number, description, sum, duty, valueVAT, sumVAT, TNVED, countryCode, uomCode, uomName));
                     }
 
-                    ImportField userNumberField = new ImportField(findProperty("userNumber[DeclarationDetail]"));
-                    ImportField nameCustomsField = new ImportField(findProperty("nameCustoms[DeclarationDetail]"));
-                    ImportField sumDataField = new ImportField(findProperty("homeSum[DeclarationDetail]"));
-                    ImportField sumDutyDataField = new ImportField(findProperty("dutySum[DeclarationDetail]"));
-                    ImportField sumVATDataField = new ImportField(findProperty("VATSum[DeclarationDetail]"));
-                    ImportField dateField = new ImportField(DateClass.instance);
-                    ImportField codeCustomsGroupField = new ImportField(findProperty("codeCustomsGroup[DeclarationDetail]"));
-                    ImportField sidOrigin2CountryField = new ImportField(findProperty("sidOrigin2Country[DeclarationDetail]"));
-                    ImportField nameUOMField = new ImportField(findProperty("name[UOM]"));
-                    ImportField UOMIDField = new ImportField(findProperty("id[UOM]"));
-
                     List<ImportProperty<?>> properties = new ArrayList<>();
+                    List<ImportField> fields = new ArrayList<>();
+                    List<ImportKey<?>> keys = new ArrayList<>();
 
+                    ImportField userNumberField = new ImportField(findProperty("userNumber[DeclarationDetail]"));
                     ImportKey<?> declarationDetailKey = new ImportKey((ConcreteCustomClass) findClass("DeclarationDetail"),
-                            findProperty("declarationDetail[Declaration,INTEGER]").getMapping(declaration, userNumberField));
+                            findProperty("declarationDetail[Declaration,INTEGER]").getMapping(declarationObject, userNumberField));
+                    keys.add(declarationDetailKey);
+                    properties.add(new ImportProperty(userNumberField, findProperty("userNumber[DeclarationDetail]").getMapping(declarationDetailKey)));
+                    properties.add(new ImportProperty(declarationObject, findProperty("declaration[DeclarationDetail]").getMapping(declarationDetailKey)));
+                    fields.add(userNumberField);
 
+                    ImportField nameCustomsField = new ImportField(findProperty("nameCustoms[DeclarationDetail]"));
+                    properties.add(new ImportProperty(nameCustomsField, findProperty("nameCustoms[DeclarationDetail]").getMapping(declarationDetailKey)));
+                    fields.add(nameCustomsField);
+
+                    ImportField sumDataField = new ImportField(findProperty("homeSum[DeclarationDetail]"));
+                    properties.add(new ImportProperty(sumDataField, findProperty("homeSum[DeclarationDetail]").getMapping(declarationDetailKey)));
+                    fields.add(sumDataField);
+
+                    ImportField sumDutyDataField = new ImportField(findProperty("dutySum[DeclarationDetail]"));
+                    properties.add(new ImportProperty(sumDutyDataField, findProperty("dutySum[DeclarationDetail]").getMapping(declarationDetailKey)));
+                    fields.add(sumDutyDataField);
+
+                    ImportField valueVATUserInvoiceDetailField = new ImportField(findProperty("percentVAT[DeclarationDetail]"));
+                    ImportKey<?> VATKey = new ImportKey((CustomClass) findClass("Range"),
+                            findProperty("valueCurrentVATDefault[NUMERIC[10,5]]").getMapping(valueVATUserInvoiceDetailField));
+                    keys.add(VATKey);
+                    properties.add(new ImportProperty(valueVATUserInvoiceDetailField, findProperty("VAT[DeclarationDetail]").getMapping(declarationDetailKey),
+                            object(findClass("Range")).getMapping(VATKey)));
+                    fields.add(valueVATUserInvoiceDetailField);
+
+                    ImportField sumVATDataField = new ImportField(findProperty("VATSum[DeclarationDetail]"));
+                    properties.add(new ImportProperty(sumVATDataField, findProperty("VATSum[DeclarationDetail]").getMapping(declarationDetailKey)));
+                    fields.add(sumVATDataField);
+
+                    ImportField codeCustomsGroupField = new ImportField(findProperty("codeCustomsGroup[DeclarationDetail]"));
                     ImportKey<?> customsGroupKey = new ImportKey((ConcreteCustomClass) findClass("CustomsGroup"),
                             findProperty("customsGroup[STRING[10]]").getMapping(codeCustomsGroupField));
-
-                    ImportKey<?> countryKey = new ImportKey((ConcreteCustomClass) findClass("Country"),
-                            findProperty("countrySIDOrigin2[STRING[2]]").getMapping(sidOrigin2CountryField));
-
-                    ImportKey<?> UOMKey = new ImportKey((ConcreteCustomClass) findClass("UOM"),
-                            findProperty("UOM[VARSTRING[100]]").getMapping(UOMIDField));
-
-                    properties.add(new ImportProperty(userNumberField, findProperty("userNumber[DeclarationDetail]").getMapping(declarationDetailKey)));
-                    properties.add(new ImportProperty(nameCustomsField, findProperty("nameCustoms[DeclarationDetail]").getMapping(declarationDetailKey)));
-                    properties.add(new ImportProperty(sumDataField, findProperty("homeSum[DeclarationDetail]").getMapping(declarationDetailKey)));
-                    properties.add(new ImportProperty(sumDutyDataField, findProperty("dutySum[DeclarationDetail]").getMapping(declarationDetailKey)));
-                    properties.add(new ImportProperty(sumVATDataField, findProperty("VATSum[DeclarationDetail]").getMapping(declarationDetailKey)));
-                    properties.add(new ImportProperty(declaration, findProperty("declaration[DeclarationDetail]").getMapping(declarationDetailKey)));
-
-                    properties.add(new ImportProperty(nameUOMField, findProperty("name[UOM]").getMapping(UOMKey)));
-                    properties.add(new ImportProperty(nameUOMField, findProperty("shortName[UOM]").getMapping(UOMKey)));
-                    properties.add(new ImportProperty(UOMIDField, findProperty("id[UOM]").getMapping(UOMKey)));
-                    properties.add(new ImportProperty(UOMIDField, findProperty("UOM[DeclarationDetail]").getMapping(declarationDetailKey),
-                            object(findClass("UOM")).getMapping(UOMKey)));
-
+                    keys.add(customsGroupKey);
                     properties.add(new ImportProperty(codeCustomsGroupField, findProperty("code[CustomsGroup]").getMapping(customsGroupKey)));
                     properties.add(new ImportProperty(codeCustomsGroupField, findProperty("customsGroup[DeclarationDetail]").getMapping(declarationDetailKey),
                             object(findClass("CustomsGroup")).getMapping(customsGroupKey)));
+                    fields.add(codeCustomsGroupField);
+
+                    ImportField sidOrigin2CountryField = new ImportField(findProperty("sidOrigin2Country[DeclarationDetail]"));
+                    ImportKey<?> countryKey = new ImportKey((ConcreteCustomClass) findClass("Country"),
+                            findProperty("countrySIDOrigin2[STRING[2]]").getMapping(sidOrigin2CountryField));
+                    keys.add(countryKey);
                     properties.add(new ImportProperty(sidOrigin2CountryField, findProperty("sidOrigin2Country[DeclarationDetail]").getMapping(declarationDetailKey)));
                     properties.add(new ImportProperty(sidOrigin2CountryField, findProperty("country[DeclarationDetail]").getMapping(declarationDetailKey),
                             object(findClass("Country")).getMapping(countryKey)));
+                    fields.add(sidOrigin2CountryField);
 
-                    List<ImportField> fields = BaseUtils.toList(userNumberField, nameCustomsField, sumDataField,
-                            sumDutyDataField, sumVATDataField, dateField, codeCustomsGroupField, sidOrigin2CountryField,
-                            nameUOMField, UOMIDField);
-                    ImportKey<?>[] keysArray = new ImportKey<?>[]{declarationDetailKey, customsGroupKey, countryKey, UOMKey};
+                    ImportField idUOMField = new ImportField(findProperty("id[UOM]"));
+                    ImportKey<?> UOMKey = new ImportKey((ConcreteCustomClass) findClass("UOM"),
+                            findProperty("UOM[VARSTRING[100]]").getMapping(idUOMField));
+                    keys.add(UOMKey);
+                    properties.add(new ImportProperty(idUOMField, findProperty("id[UOM]").getMapping(UOMKey)));
+                    properties.add(new ImportProperty(idUOMField, findProperty("UOM[DeclarationDetail]").getMapping(declarationDetailKey),
+                            object(findClass("UOM")).getMapping(UOMKey)));
+                    fields.add(idUOMField);
 
-                    IntegrationService integrationService = new IntegrationService(context.getSession(), new ImportTable(fields, data), Arrays.asList(keysArray), properties);
+                    ImportField nameUOMField = new ImportField(findProperty("name[UOM]"));
+                    properties.add(new ImportProperty(nameUOMField, findProperty("name[UOM]").getMapping(UOMKey)));
+                    properties.add(new ImportProperty(nameUOMField, findProperty("shortName[UOM]").getMapping(UOMKey)));
+                    fields.add(nameUOMField);
+
+                    IntegrationService integrationService = new IntegrationService(context.getSession(), new ImportTable(fields, data), keys, properties);
                     integrationService.synchronize(true, false);
                 }
             }
         } catch (IOException | ScriptingErrorLog.SemanticErrorException | SQLException | JDOMException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            throw Throwables.propagate(e);
+        }
+    }
+
+    private BigDecimal parseBigDecimal(String value) {
+        try {
+            return value == null ? null : new BigDecimal(trim(value).replace(",", "."));
+        } catch (NumberFormatException e) {
+            return null;
         }
     }
 }
