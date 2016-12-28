4444bebccaaa3aa6c645ad7af2c89ddfcb2e81dc
AFilipchyk <filipchyk.andrei@gmail.com>
2016-12-28 07:26:40 +0000

    [equ-srv] Генерация чеков: добавлены дисконтные карты

--- equ-srv/src/main/java/equ/srv/actions/GenerateZReport.java
+++ equ-srv/src/main/java/equ/srv/actions/GenerateZReport.java
@@ -6,6 +6,7 @@ import lsfusion.base.col.MapFact;
 import lsfusion.base.col.interfaces.immutable.ImMap;
 import lsfusion.base.col.interfaces.immutable.ImOrderMap;
 import lsfusion.base.col.interfaces.immutable.ImRevMap;
+import lsfusion.erp.integration.DefaultIntegrationActionProperty;
 import lsfusion.interop.Compare;
 import lsfusion.server.data.SQLHandledException;
 import lsfusion.server.data.expr.KeyExpr;
@@ -13,28 +14,29 @@ import lsfusion.server.data.query.QueryBuilder;
 import lsfusion.server.logics.DataObject;
 import lsfusion.server.logics.NullValue;
 import lsfusion.server.logics.ObjectValue;
-import lsfusion.server.logics.linear.LCP;
 import lsfusion.server.logics.property.ClassPropertyInterface;
 import lsfusion.server.logics.property.ExecutionContext;
-import lsfusion.server.logics.property.PropertyInterface;
-import lsfusion.server.logics.scripted.ScriptingActionProperty;
 import lsfusion.server.logics.scripted.ScriptingErrorLog;
 import lsfusion.server.logics.scripted.ScriptingLogicsModule;
 import lsfusion.server.session.DataSession;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.sql.Date;
 import java.sql.SQLException;
 import java.sql.Time;
+import java.sql.Timestamp;
 import java.util.*;
 
-public class GenerateZReport extends ScriptingActionProperty {
+public class GenerateZReport extends DefaultIntegrationActionProperty {
+    private static final double deviationPercent = 0.25;
+
     public GenerateZReport(ScriptingLogicsModule LM) {
         super(LM);
     }
 
     @Override
-    protected void executeCustom(ExecutionContext<ClassPropertyInterface> context) throws SQLException, SQLHandledException {
+    public void executeCustom(ExecutionContext<ClassPropertyInterface> context) throws SQLException, SQLHandledException {
 
         DataSession session = context.getSession();
         List<SalesInfo> salesInfoList = new ArrayList<>();
@@ -42,15 +44,15 @@ public class GenerateZReport extends ScriptingActionProperty {
             ObjectValue priceListType = findProperty("priceListTypeGenerateZReport[]").readClasses(context);
             if (!(priceListType instanceof NullValue)) {
                 Random r = new Random();
-                Integer zReportCount = addDeviation((Integer) findProperty("averageZReportCountGenerateZReport[]").read(context), 0.25, r);
+                Integer zReportCount = addDeviation((Integer) findProperty("averageZReportCountGenerateZReport[]").read(context), r);
                 Integer receiptCount = (Integer) findProperty("averageReceiptCountGenerateZReport[]").read(context);
                 Integer receiptDetailCount = (Integer) findProperty("averageReceiptDetailCountGenerateZReport[]").read(context);
                 ObjectValue stockObject = findProperty("departmentStoreGenerateZReport[]").readClasses(context);
 
-                Date dateFrom = (Date) findProperty("dateFromGenerateZReport[]").read(context);
-                dateFrom = dateFrom == null ? new Date(System.currentTimeMillis()) : dateFrom;
-                Date dateTo = (Date) findProperty("dateToGenerateZReport[]").read(context);
-                dateTo = dateTo == null ? new Date(System.currentTimeMillis()) : dateTo;
+                Timestamp dateFrom = (Timestamp) findProperty("dateFromGenerateZReport[]").read(context);
+                dateFrom = dateFrom == null ? new Timestamp(System.currentTimeMillis()) : dateFrom;
+                Timestamp dateTo = (Timestamp) findProperty("dateToGenerateZReport[]").read(context);
+                dateTo = dateTo == null ? new Timestamp(System.currentTimeMillis()) : dateTo;
 
                 KeyExpr departmentStoreExpr = new KeyExpr("departmentStore");
                 KeyExpr itemExpr = new KeyExpr("item");
@@ -109,6 +111,19 @@ public class GenerateZReport extends ScriptingActionProperty {
                     }
                 }
 
+                List<String> discountCardList = new ArrayList<>();
+                KeyExpr discountCardExpr = new KeyExpr("discountCard");
+
+                ImRevMap<Object, KeyExpr> keys = MapFact.singletonRev((Object) "discountCard", discountCardExpr);
+                QueryBuilder<Object, Object> discountCardQuery = new QueryBuilder<>(keys);
+                discountCardQuery.addProperty("seriesNumber", findProperty("seriesNumber[DiscountCard]").getExpr(discountCardExpr));
+                discountCardQuery.and(findProperty("seriesNumber[DiscountCard]").getExpr(discountCardExpr).getWhere());
+
+                ImOrderMap<ImMap<Object, Object>, ImMap<Object, Object>> discountCardResult = discountCardQuery.execute(session);
+                for(ImMap<Object, Object> entry : discountCardResult.values()) {
+                    discountCardList.add((String) entry.get("seriesNumber"));
+                }
+
                 if (!cashRegisterInfoList.isEmpty()) {
                     Map<String, DataObject> numberZReportCashRegisterMap = new HashMap<>();
 
@@ -120,43 +135,49 @@ public class GenerateZReport extends ScriptingActionProperty {
                             numberZReport = String.valueOf((cashRegister.maxNumberZReport == null ? 0 : cashRegister.maxNumberZReport) + (zReportCount < 1 ? 0 : r.nextInt(zReportCount)) + 1);
                         if (!numberZReportCashRegisterMap.containsKey(numberZReport))
                             numberZReportCashRegisterMap.put(numberZReport, cashRegister.cashRegisterObject);
-                        java.sql.Date date = new java.sql.Date(dateFrom.getTime() + Math.abs(r.nextLong() % (dateTo.getTime() - dateFrom.getTime())));
-                        for (int receiptNumber = 1; receiptNumber <= addDeviation(receiptCount, 0.25, r); receiptNumber++) {
+                        Timestamp dateTime = dateFrom.getTime() <= dateTo.getTime() ? dateFrom : (new Timestamp(dateFrom.getTime() + Math.abs(r.nextLong() % (dateTo.getTime() - dateFrom.getTime()))));
+                        Date date = new Date(dateTime.getTime());
+                        BigDecimal discountSum = null;
+                        for (int receiptNumber = 1; receiptNumber <= addDeviation(receiptCount, r); receiptNumber++) {
 
                             Integer numberReceiptDetail = 0;
                             BigDecimal sumCash = BigDecimal.ZERO;
                             List<SalesInfo> receiptSalesInfoList = new ArrayList<>();
 
-                            Time time = new Time(r.nextLong() % date.getTime());
-                            Integer currentReceiptDetailCount = addDeviation(receiptDetailCount, 0.25, r);
+                            Time time = new Time(r.nextLong() % dateTime.getTime());
+                            Integer currentReceiptDetailCount = addDeviation(receiptDetailCount, r);
                             Set<Integer> usedItems = new HashSet<>();
                             while(currentReceiptDetailCount >= numberReceiptDetail && usedItems.size() < itemZReportInfoList.size()) {
                                 int currentItemIndex = r.nextInt(itemZReportInfoList.size());
-                                ItemZReportInfo itemZReportInfo = itemZReportInfoList.get(currentItemIndex);
-                                BigDecimal currentBalanceSkuStock = itemZReportInfo.count;
-                                if ((currentBalanceSkuStock.doubleValue() > 0) && (cashRegister.departmentStoreObject.equals(itemZReportInfo.departmentStore))) {
+                                ItemZReportInfo item = itemZReportInfoList.get(currentItemIndex);
+                                BigDecimal currentBalanceSkuStock = item.count;
+                                if ((currentBalanceSkuStock.doubleValue() > 0) && (cashRegister.departmentStoreObject.equals(item.departmentStore))) {
                                     BigDecimal quantityReceiptDetail;
-                                    if (itemZReportInfo.splitItem)
+                                    if (item.splitItem)
                                         quantityReceiptDetail = currentBalanceSkuStock.doubleValue() <= 0.005 ? currentBalanceSkuStock : BigDecimal.valueOf((double) Math.round(r.nextDouble() * currentBalanceSkuStock.doubleValue() / 5 * 1000) / 1000);
                                     else
                                         quantityReceiptDetail = BigDecimal.valueOf(Math.ceil(currentBalanceSkuStock.doubleValue() / 5) == 1 ? 1.0 : r.nextInt((int) Math.ceil(currentBalanceSkuStock.doubleValue() / 5)));
                                     if ((quantityReceiptDetail.doubleValue() > 0) && (currentReceiptDetailCount >= numberReceiptDetail)) {
-                                        BigDecimal sumReceiptDetail = quantityReceiptDetail.multiply(itemZReportInfo.price == null ? BigDecimal.ZERO : itemZReportInfo.price);
+                                        BigDecimal sumReceiptDetail = item.price == null ? null : safeMultiply(quantityReceiptDetail, item.price);
                                         numberReceiptDetail++;
                                         sumCash = safeAdd(sumCash, sumReceiptDetail);
-                                        BigDecimal discountSumReceiptDetail = BigDecimal.valueOf(r.nextDouble() > 0.8 ? (sumReceiptDetail.doubleValue() * r.nextInt(10) / 100) : 0);
-                                        SalesInfo salesInfo = new SalesInfo(false, cashRegister.nppGroupMachinery, cashRegister.nppMachinery, numberZReport, date, time, receiptNumber, date,
-                                                time, null, null, null, BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO, itemZReportInfo.barcode, null, null,
-                                                null, quantityReceiptDetail, itemZReportInfo.price, sumReceiptDetail, discountSumReceiptDetail, null, null,
-                                                numberReceiptDetail, null, null);
-                                        receiptSalesInfoList.add(salesInfo);
-                                        itemZReportInfo.count = safeSubtract(itemZReportInfo.count, quantityReceiptDetail);
+                                        BigDecimal discountSumReceiptDetail = r.nextDouble() > 0.8 ? safeDivide(safeMultiply(sumReceiptDetail, r.nextInt(10)), 100) : BigDecimal.ZERO;
+                                        discountSum = safeAdd(discountSum, discountSumReceiptDetail);
+                                        receiptSalesInfoList.add(new SalesInfo(false, cashRegister.nppGroupMachinery, cashRegister.nppMachinery,
+                                                numberZReport, date, time, receiptNumber, date, time, null, null, null, BigDecimal.ZERO,
+                                                BigDecimal.ZERO, BigDecimal.ZERO, item.barcode, null, null, null, quantityReceiptDetail,
+                                                item.price, sumReceiptDetail, discountSumReceiptDetail, null, null, numberReceiptDetail,
+                                                null, null));
+                                        item.count = safeSubtract(item.count, quantityReceiptDetail);
                                     }
                                 }
                                 usedItems.add(currentItemIndex);
                             }
+
+                            String seriesNumberDiscountCard = discountSum != null ? discountCardList.get(r.nextInt(discountCardList.size())) : null;
                             for (SalesInfo s : receiptSalesInfoList) {
                                 s.sumCash = sumCash;
+                                s.seriesNumberDiscountCard = seriesNumberDiscountCard;
                             }
                             salesInfoList.addAll(receiptSalesInfoList);
                         }
@@ -177,27 +198,10 @@ public class GenerateZReport extends ScriptingActionProperty {
         }
     }
 
-    private Integer addDeviation(Integer value, double percent, Random r) {
-        return value != null ? value + (int) (value * r.nextDouble() * percent * (r.nextDouble() > 0.5 ? 1 : -1)) : 1;
+    private Integer addDeviation(Integer value, Random r) {
+        return value != null ? value + (int) (value * r.nextDouble() * deviationPercent * (r.nextDouble() > 0.5 ? 1 : -1)) : 1;
     }
 
-    private BigDecimal safeAdd(BigDecimal operand1, BigDecimal operand2) {
-        if (operand1 == null && operand2 == null)
-            return null;
-        else return (operand1 == null ? operand2 : (operand2 == null ? operand1 : operand1.add(operand2)));
-    }
-    
-    private BigDecimal safeSubtract(BigDecimal operand1, BigDecimal operand2) {
-        if (operand1 == null && operand2 == null)
-            return null;
-        else
-            return (operand1 == null ? operand2.negate() : (operand2 == null ? operand1 : operand1.subtract((operand2))));
-    }
-
-    protected String trim(String input) {
-        return input == null ? null : input.trim();
-    }
-    
     private class ItemZReportInfo {
         String barcode;
         BigDecimal count;
