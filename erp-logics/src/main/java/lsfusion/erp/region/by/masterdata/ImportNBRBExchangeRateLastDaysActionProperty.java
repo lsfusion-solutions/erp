346bdb8457dd600635423af0320e6162296da9b0
Eveigh <varets.p@gmail.com>
2017-09-05 17:05:32 +0000

    [erp] при импорте последних курсов смотрим на день вперед

--- erp-logics/src/main/java/lsfusion/erp/region/by/masterdata/ImportNBRBExchangeRateLastDaysActionProperty.java
+++ erp-logics/src/main/java/lsfusion/erp/region/by/masterdata/ImportNBRBExchangeRateLastDaysActionProperty.java
@@ -41,7 +41,7 @@ public class ImportNBRBExchangeRateLastDaysActionProperty extends ImportNBRBExch
                 Date separationDate = new Date(2016 - 1900, 5, 30); //30.06.2016
                 Date denominationDate = new Date(2016 - 1900, 6, 1); //01.07.2016
                 Date dateFrom = new Date(currentTime - (long) days * 24 * 3600 * 1000);
-                Date dateTo = new Date(currentTime);
+                Date dateTo = new Date(currentTime + (long) 24 * 3600 * 1000);
                 if(dateFrom.getTime() <= separationDate.getTime() && dateTo.getTime() > separationDate.getTime()) {
                     importExchanges(dateFrom, separationDate, shortNameCurrency, context, true);
                     importExchanges(denominationDate, dateTo, shortNameCurrency, context, false);
