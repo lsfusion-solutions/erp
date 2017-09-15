fbfa4f2e36347ccf4f4d49b16792ba9636d42c1d
AFilipchyk <filipchyk.andrei@gmail.com>
2017-09-15 09:17:55 +0000

    [equ] Artix: из settings убрана опция discountCardNames

--- equ-clt/src/main/java/equ/clt/handler/artix/ArtixSettings.java
+++ equ-clt/src/main/java/equ/clt/handler/artix/ArtixSettings.java
@@ -1,16 +1,12 @@
 package equ.clt.handler.artix;
 
 import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
 
 public class ArtixSettings implements Serializable{
 
     private String globalExchangeDirectory;
     private boolean appendBarcode;
     private String giftCardRegexp;
-    private String discountCardNames;
-    private Map<String, String> discountCardNamesMap = new HashMap();
 
     public ArtixSettings() {
     }
@@ -37,26 +33,5 @@ public class ArtixSettings implements Serializable{
 
     public void setGiftCardRegexp(String giftCardRegexp) {
         this.giftCardRegexp = giftCardRegexp;
-    }
-
-    public void setDiscountCardNames(String discountCardNames) {
-        this.discountCardNames = discountCardNames;
-        this.discountCardNamesMap = new HashMap<>();
-        if(!discountCardNames.isEmpty()) {
-            String[] entries = discountCardNames.split(",\\s?");
-            for (String entry : entries) {
-                String[] percentType = entry.split("->");
-                if(percentType.length == 2) {
-                    try {
-                        this.discountCardNamesMap.put(percentType[0], percentType[1]);
-                    } catch (Exception ignored) {
-                    }
-                }
-            }
-        }
-    }
-
-    public Map<String, String> getDiscountCardNamesMap() {
-        return discountCardNamesMap;
     }
 }
