d5c9aed500231fe96e9ad5f6788bd337ccc61d3d
AFilipchyk <filipchyk.andrei@gmail.com>
2017-09-20 07:56:15 +0000

    [erp] Board: текст по умолчанию для табло

--- erp-logics/src/main/java/lsfusion/erp/region/by/machinery/board/fiscalboard/FiscalBoardResetTextActionProperty.java
+++ erp-logics/src/main/java/lsfusion/erp/region/by/machinery/board/fiscalboard/FiscalBoardResetTextActionProperty.java
@@ -12,6 +12,8 @@ import lsfusion.server.logics.scripted.ScriptingLogicsModule;
 import java.sql.SQLException;
 import java.util.Iterator;
 
+import static lsfusion.base.BaseUtils.trimToEmpty;
+
 public class FiscalBoardResetTextActionProperty extends ScriptingActionProperty {
     private final ClassPropertyInterface receiptInterface;
 
@@ -31,16 +33,27 @@ public class FiscalBoardResetTextActionProperty extends ScriptingActionProperty 
             if (!skipReceipt) {
                 Integer comPortBoard = (Integer) findProperty("comPortBoardCurrentCashRegister[]").read(context);
                 Integer baudRateBoard = (Integer) findProperty("baudRateBoardCurrentCashRegister[]").read(context);
+                String defaultTextBoard = trimToEmpty((String) findProperty("defaultTextBoard[]").read(context));
 
-                String line = "";
-                for (int i = 0; i < 20; i++)
-                    line += " ";
+                String[] lines = generateText(defaultTextBoard, 20);
 
-                context.requestUserInteraction(new FiscalBoardDisplayTextClientAction(line, line, baudRateBoard, comPortBoard));
+                context.requestUserInteraction(new FiscalBoardDisplayTextClientAction(lines[0], lines[1], baudRateBoard, comPortBoard));
 
             }
         } catch (SQLException | ScriptingErrorLog.SemanticErrorException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static String[] generateText(String text, int len) {
+        String firstLine = fillSpaces(text.substring(0, Math.min(len, text.length())), len);
+        String secondLine = fillSpaces(text.substring(Math.min(len, text.length()), Math.min(len * 2, text.length())), len);
+        return new String[]{firstLine, secondLine};
+    }
+
+    private static String fillSpaces(String value, int length) {
+        while (value.length() < length)
+            value += " ";
+        return value;
     }
 }
