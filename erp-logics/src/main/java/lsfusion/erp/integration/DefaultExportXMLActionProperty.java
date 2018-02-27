05b6865067efc16b8835177822d43e01246cd182
AFilipchyk <filipchyk.andrei@gmail.com>
2018-02-27 07:05:26 +0000

    [sosedi] ExportBPM: промежуточная версия (refs #5622)

--- erp-logics/src/main/java/lsfusion/erp/integration/DefaultExportXMLActionProperty.java
+++ erp-logics/src/main/java/lsfusion/erp/integration/DefaultExportXMLActionProperty.java
@@ -1,15 +1,24 @@
 package lsfusion.erp.integration;
 
+import lsfusion.server.ServerLoggers;
 import lsfusion.server.classes.ValueClass;
 import lsfusion.server.logics.scripted.ScriptingLogicsModule;
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import org.apache.commons.io.FileUtils;
 import org.jdom.*;
 import org.jdom.output.Format;
 import org.jdom.output.XMLOutputter;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.Writer;
 import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class DefaultExportXMLActionProperty extends DefaultExportActionProperty {
 
@@ -80,5 +89,22 @@ public class DefaultExportXMLActionProperty extends DefaultExportActionProperty 
     protected void addStringElement(Namespace namespace, Element parent, String id, String value) {
         if (value != null)
             parent.addContent(new Element(id, namespace).setText(value));
+    }
+
+    protected String outputXML(Document doc, String encoding, String outputDir) {
+        String xml = new XMLOutputter(Format.getPrettyFormat().setEncoding(encoding)).outputString(doc);
+        if (outputDir != null) {
+            try {
+                FileUtils.writeStringToFile(new File(outputDir + "/" + new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss").format(Calendar.getInstance().getTime()) + ".xml"), xml);
+            } catch (Exception e) {
+                ServerLoggers.importLogger.error("Export Error: ", e);
+            }
+        }
+        return xml;
+    }
+
+    protected String sendRequest(String url, String xml) throws IOException {
+        Request request = new Request.Builder().url(url).post(FormBody.create(MediaType.parse("application/xml"), xml)).build();
+        return new OkHttpClient().newCall(request).execute().body().string();
     }
 }
