de3536635b7bf335dabde3ef507144b5a598c5e8
CrushBy <crushby@gmail.com>
2018-04-19 13:26:19 +0000

    Мелкие изменения в экспорте в Artix

--- equ-clt/src/main/java/equ/clt/handler/artix/ArtixHandler.java
+++ equ-clt/src/main/java/equ/clt/handler/artix/ArtixHandler.java
@@ -266,7 +266,7 @@ public class ArtixHandler extends DefaultCashRegisterHandler<ArtixSalesBatch> {
                     barcodeObject.put("additionalprices", new JSONArray());
                     barcodeObject.put("barcode", removeCheckDigitFromBarcode(barcode.idBarcode, appendBarcode));
                     barcodeObject.put("cquant", barcode.amountBarcode);
-                    barcodeObject.put("measurecode", idUOM);
+                    barcodeObject.put("measurecode", barcode.idUOM);
                     barcodeObject.put("minprice",  barcode.flags == null || ((barcode.flags & 16) == 0) ? barcode.price : barcode.minPrice != null ? barcode.minPrice : BigDecimal.ZERO);
                     barcodeObject.put("name", barcode.name);
                     barcodeObject.put("price", barcode.price);
