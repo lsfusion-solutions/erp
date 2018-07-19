b68dc08e1b98cac6439fdd5dc5bc7373964f2df2
AFilipchyk <filipchyk.andrei@gmail.com>
2018-07-19 07:16:54 +0000

    [mothercare] httpserver.log

--- erp-logics/src/main/java/lsfusion/erp/ERPLoggers.java
+++ erp-logics/src/main/java/lsfusion/erp/ERPLoggers.java
@@ -3,5 +3,9 @@ package lsfusion.erp;
 import org.apache.log4j.Logger;
 
 public class ERPLoggers {
+
     public static final Logger terminalLogger = Logger.getLogger("TerminalLogger");
+
+    public static final Logger httpServerLogger = Logger.getLogger("HttpServerLogger");
+
 }
