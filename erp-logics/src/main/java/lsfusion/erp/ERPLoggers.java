e6d443eca14b2d68e635b6ef0da4ad201776c8de
AFilipchyk <filipchyk.andrei@gmail.com>
2018-07-31 06:27:16 +0000

    [vitalur] httpserver.log, set.log

--- erp-logics/src/main/java/lsfusion/erp/ERPLoggers.java
+++ erp-logics/src/main/java/lsfusion/erp/ERPLoggers.java
@@ -3,9 +3,5 @@ package lsfusion.erp;
 import org.apache.log4j.Logger;
 
 public class ERPLoggers {
-
     public static final Logger terminalLogger = Logger.getLogger("TerminalLogger");
-
-    public static final Logger httpServerLogger = Logger.getLogger("HttpServerLogger");
-
 }
