681da8be4b3a99fcd852c6ee54c90d09c90ffe58
AFilipchyk <filipchyk.andrei@gmail.com>
2018-11-26 08:41:02 +0000

    [equ] BelCoopSoyuz SQL: приём реализации

--- equ-clt/src/main/java/equ/clt/handler/belcoopsoyuz/BelCoopSoyuzSQLHandler.java
+++ equ-clt/src/main/java/equ/clt/handler/belcoopsoyuz/BelCoopSoyuzSQLHandler.java
@@ -5,6 +5,7 @@ import equ.api.SalesBatch;
 import equ.api.SalesInfo;
 import equ.api.SendTransactionBatch;
 import equ.api.cashregister.CashRegisterInfo;
+import equ.api.cashregister.CashRegisterItemInfo;
 import equ.api.cashregister.TransactionCashRegisterInfo;
 import equ.clt.handler.DefaultCashRegisterHandler;
 import org.apache.log4j.Logger;
@@ -16,8 +17,7 @@ import java.sql.*;
 import java.sql.Date;
 import java.util.*;
 
-import static equ.clt.handler.HandlerUtils.safeAdd;
-import static equ.clt.handler.HandlerUtils.safeNegate;
+import static equ.clt.handler.HandlerUtils.*;
 
 public class BelCoopSoyuzSQLHandler extends DefaultCashRegisterHandler<BelCoopSoyuzSQLSalesBatch> {
 
@@ -38,8 +38,113 @@ public class BelCoopSoyuzSQLHandler extends DefaultCashRegisterHandler<BelCoopSo
 
     @Override
     public Map<Long, SendTransactionBatch> sendTransaction(List<TransactionCashRegisterInfo> transactionList) throws IOException {
-        //todo
-        return new HashMap<>();
+        Map<Long, SendTransactionBatch> sendTransactionBatchMap = new HashMap<>();
+
+        if (transactionList != null) {
+
+            try {
+
+                Class.forName("oracle.jdbc.driver.OracleDriver");
+
+                for (TransactionCashRegisterInfo transaction : transactionList) {
+
+                    String directory = null;
+                    for (CashRegisterInfo cashRegister : transaction.machineryInfoList) {
+                        if (cashRegister.directory != null) {
+                            directory = cashRegister.directory;
+                        }
+                    }
+
+                    if (directory == null) {
+                        processTransactionLogger.error(logPrefix + "No connectionString found");
+                    } else {
+                        processTransactionLogger.info(String.format(logPrefix + "connecting to %s", directory));
+                        Exception exception = null;
+                        try (Connection conn = DriverManager.getConnection(directory)) {
+                            processTransactionLogger.info(String.format(logPrefix + "transaction %s, table plu", transaction.id));
+                            exportItems(conn, transaction);
+                        } catch (Exception e) {
+                            exception = e;
+                        }
+                        sendTransactionBatchMap.put(transaction.id, new SendTransactionBatch(exception));
+                    }
+                }
+            } catch (ClassNotFoundException e) {
+                throw Throwables.propagate(e);
+            }
+        }
+        return sendTransactionBatchMap;
+    }
+
+    private void exportItems(Connection conn, TransactionCashRegisterInfo transaction) throws SQLException {
+        if (transaction.itemsList != null) {
+            PreparedStatement ps = null;
+            try {
+//                if(transaction.snapshot) { //TODO: пока нам всё равно не дают на это прав
+//                    conn.setAutoCommit(true);
+//                    try(Statement truncateStatement = conn.createStatement()) {
+//                        if (snapshot) {
+//                            conn.createStatement().execute("TRUNCATE TABLE cl1_bks.a9sk34");
+//                        }
+//                    }
+//                }
+                conn.setAutoCommit(false);
+                ps = conn.prepareStatement("MERGE INTO cl1_bks.l9sk34 dest " +
+                                "USING(SELECT ? CEUNIKEY, ? CEUNIREF0, ? CEDOCCOD, ? CEOBIDE, ? CEOBMEA, ? MEOBNAM, ? NERECOST, ? NEOPPRIC, ? TEDOCACT, ? CESUCOD FROM DUAL) src " +
+                                "ON (dest.CEUNIKEY = src.CEUNIKEY) " +
+                                "WHEN MATCHED THEN " +
+                                "UPDATE SET dest.CEUNIREF0=src.CEUNIREF0, dest.CEDOCCOD=src.CEDOCCOD, dest.CEOBIDE=src.CEOBIDE, dest.CEOBMEA=src.CEOBMEA, dest.MEOBNAM=src.MEOBNAM, dest.NERECOST=src.NERECOST, dest.NEOPPRIC=src.NEOPPRIC, dest.TEDOCACT=src.TEDOCACT, dest.CESUCOD=src.CESUCOD " +
+                                "WHEN NOT MATCHED THEN " +
+                                "INSERT (CEUNIKEY, CEUNIREF0, CEDOCCOD, CEOBIDE, CEOBMEA, MEOBNAM, NERECOST, NEOPPRIC, TEDOCACT, CESUCOD) " +
+                                "VALUES (src.CEUNIKEY, src.CEUNIREF0, src.CEDOCCOD, src.CEOBIDE, src.CEOBMEA, src.MEOBNAM, src.NERECOST, src.NEOPPRIC, src.TEDOCACT, src.CESUCOD)"
+                );
+
+                for (CashRegisterItemInfo item : transaction.itemsList) {
+                    String barcode = appendBarcode(item.idBarcode);
+                    ps.setString(1, appendSpaces(barcode, 25)); //CEUNIKEY (type NCHAR 25), уникальный ключ записи в базе данных
+                    ps.setString(2, trim(transaction.idDepartmentStoreGroupCashRegister, 25)); //CEUNIREF0, УНП предприятия
+                    ps.setString(3, "ПРАЙС"); //CEDOCCOD, тип записи
+                    ps.setString(4, trim(barcode, 25)); //CEOBIDE, Штрих-код товара
+                    ps.setString(5, trim(item.shortNameUOM, 25)); //CEOBMEA, единица измерения
+                    ps.setString(6, trim(item.name, 100)); //MEOBNAM, Наименование товара
+                    ps.setDouble(7, item.price.doubleValue()); //NERECOST, роз.цена
+                    ps.setDouble(8, item.price.doubleValue()); //NEOPPRIC, роз.цена
+                    ps.setTimestamp(9, new java.sql.Timestamp(transaction.date.getTime())); //TEDOCACT, дата и время вступления прайса в силу
+                    ps.setString(10, item.section); //CESUCOD, Идентификатор секции
+                    ps.addBatch();
+                }
+
+                ps.executeBatch();
+                conn.commit();
+            } catch (Exception e) {
+                throw Throwables.propagate(e);
+            } finally {
+                if (ps != null)
+                    ps.close();
+            }
+        }
+    }
+
+    private String appendBarcode(String barcode) {
+        if (barcode != null) {
+            if (barcode.length() == 5)
+                barcode = "22" + barcode + "00000";
+            if (barcode.length() == 12) {
+                try {
+                    int checkSum = 0;
+                    for (int i = 0; i <= 10; i = i + 2) {
+                        checkSum += Integer.valueOf(String.valueOf(barcode.charAt(i)));
+                        checkSum += Integer.valueOf(String.valueOf(barcode.charAt(i + 1))) * 3;
+                    }
+                    checkSum %= 10;
+                    if (checkSum != 0)
+                        checkSum = 10 - checkSum;
+                    barcode = barcode.concat(String.valueOf(checkSum));
+                } catch (Exception ignored) {
+                }
+            }
+        }
+        return barcode;
     }
 
     @Override
@@ -82,7 +187,7 @@ public class BelCoopSoyuzSQLHandler extends DefaultCashRegisterHandler<BelCoopSo
         Set<String> readRecordSet = new HashSet<>();
 
         try (Statement statement = conn.createStatement()) {
-            String query = "SELECT CEUNIKEY, CEUNIREF0, CEUNIGO, CEDOCCOD, CEDOCNUM, TEDOCINS, CEOBIDE, CEOBNAM, CEOBMEA, CEOBTYP, NEOPEXP, NEOPPRIC, NEOPSUMC, " +
+            String query = "SELECT CEUNIKEY, CEUNIGO, CEDOCCOD, CEDOCNUM, TEDOCINS, CEOBIDE, CEOBNAM, CEOBMEA, CEOBTYP, NEOPEXP, NEOPPRIC, NEOPSUMC, " +
                     "NEOPDEL, NEOPPDELC, NEOPSDELC, NEOPNDS, NEOPSUMCT, CEOPDEV, CEOPMAN, CESUCOD FROM cl1_bks.a9ck07 WHERE CEUNIFOL != '0000000000000000000000001' ORDER BY CEUNIREF0, CEDOCCOD, CEUNIKEY";
             ResultSet rs = statement.executeQuery(query);
 
@@ -90,6 +195,7 @@ public class BelCoopSoyuzSQLHandler extends DefaultCashRegisterHandler<BelCoopSo
             List<SalesInfo> currentSalesInfoList = new ArrayList<>();
             Set<String> currentReadRecordSet = new HashSet<>();
             BigDecimal extraDiscountSum = null;
+            boolean error = false;
             while (rs.next()) {
 
                 String id = rs.getString("CEUNIKEY");
@@ -116,25 +222,28 @@ public class BelCoopSoyuzSQLHandler extends DefaultCashRegisterHandler<BelCoopSo
                             Integer nppMachinery = cashRegister == null ? null : cashRegister.number;
                             Integer nppGroupMachinery = cashRegister == null ? null : cashRegister.numberGroup;
 
-                            String barcodeItem = rs.getString("CEOBIDE");
-                            String idEmployee = rs.getString("CEOPDEV");
-                            BigDecimal quantityReceiptDetail = rs.getBigDecimal("NEOPEXP");
-                            BigDecimal priceReceiptDetail = rs.getBigDecimal("NEOPPRIC");
+                            if(cashRegister != null) {
+                                String barcodeItem = rs.getString("CEOBIDE");
+                                String idEmployee = rs.getString("CEOPDEV");
+                                BigDecimal quantityReceiptDetail = rs.getBigDecimal("NEOPEXP");
+                                BigDecimal priceReceiptDetail = rs.getBigDecimal("NEOPPRIC");
 
-                            BigDecimal discountSum1ReceiptDetail = rs.getBigDecimal("NEOPSDELC");
-                            BigDecimal discountSum2ReceiptDetail = rs.getBigDecimal("NEOPPDELC");
-                            BigDecimal discountSumReceiptDetail = safeNegate(safeAdd(discountSum1ReceiptDetail, discountSum2ReceiptDetail));
+                                BigDecimal discountSum1ReceiptDetail = rs.getBigDecimal("NEOPSDELC");
+                                BigDecimal discountSum2ReceiptDetail = rs.getBigDecimal("NEOPPDELC");
+                                BigDecimal discountSumReceiptDetail = safeNegate(safeAdd(discountSum1ReceiptDetail, discountSum2ReceiptDetail));
 
-                            Integer numberReceiptDetail = numberReceiptDetailMap.get(numberReceipt);
-                            numberReceiptDetail = numberReceiptDetail == null ? 1 : (numberReceiptDetail + 1);
-                            numberReceiptDetailMap.put(numberReceipt, numberReceiptDetail);
+                                Integer numberReceiptDetail = numberReceiptDetailMap.get(numberReceipt);
+                                numberReceiptDetail = numberReceiptDetail == null ? 1 : (numberReceiptDetail + 1);
+                                numberReceiptDetailMap.put(numberReceipt, numberReceiptDetail);
 
-                            BigDecimal quantity = isSale ? quantityReceiptDetail : safeNegate(quantityReceiptDetail);
-                            BigDecimal sum = isSale ? sumReceiptDetail : safeNegate(sumReceiptDetail);
-                            currentSalesInfoList.add(new SalesInfo(false, nppGroupMachinery, nppMachinery, numberZReport, dateReceipt, timeReceipt,
-                                    numberReceipt, dateReceipt, timeReceipt, idEmployee, null, null, null, null, null, barcodeItem, null, null, null,
-                                    quantity, priceReceiptDetail, sum, discountSumReceiptDetail, null, null, numberReceiptDetail, null, section, cashRegister));
-                            currentReadRecordSet.add(id);
+                                BigDecimal quantity = isSale ? quantityReceiptDetail : safeNegate(quantityReceiptDetail);
+                                BigDecimal sum = isSale ? sumReceiptDetail : safeNegate(sumReceiptDetail);
+                                currentSalesInfoList.add(new SalesInfo(false, nppGroupMachinery, nppMachinery, numberZReport, dateReceipt, timeReceipt, numberReceipt, dateReceipt, timeReceipt, idEmployee, null, null, null, null, null, barcodeItem, null, null, null, quantity, priceReceiptDetail, sum, discountSumReceiptDetail, null, null, numberReceiptDetail, null, section, cashRegister));
+                                currentReadRecordSet.add(id);
+                            } else {
+                                sendSalesLogger.error(logPrefix + "unknown section: " + section);
+                                error = true;
+                            }
                             break;
                         }
                         case "БОНУС": {
@@ -143,17 +252,20 @@ public class BelCoopSoyuzSQLHandler extends DefaultCashRegisterHandler<BelCoopSo
                         }
                         case "ВСЕГО":
                         case "ВОЗВРАТ": {
-                            boolean isSale = type.equals("ВСЕГО");
-                            for (SalesInfo salesInfo : currentSalesInfoList) {
-                                salesInfo.sumCash = isSale ? sumReceiptDetail : safeNegate(sumReceiptDetail);
-                                salesInfo.discountSumReceipt = safeAdd(salesInfo.discountSumReceipt, extraDiscountSum);
-                                salesInfoList.add(salesInfo);
+                            if(!error) {
+                                boolean isSale = type.equals("ВСЕГО");
+                                for (SalesInfo salesInfo : currentSalesInfoList) {
+                                    salesInfo.sumCash = isSale ? sumReceiptDetail : safeNegate(sumReceiptDetail);
+                                    salesInfo.discountSumReceipt = safeAdd(salesInfo.discountSumReceipt, extraDiscountSum);
+                                    salesInfoList.add(salesInfo);
+                                }
+                                readRecordSet.addAll(currentReadRecordSet);
+                                readRecordSet.add(id);
                             }
                             currentSalesInfoList = new ArrayList<>();
-                            readRecordSet.addAll(currentReadRecordSet);
                             currentReadRecordSet = new HashSet<>();
                             extraDiscountSum = null;
-                            currentReadRecordSet.add(id);
+                            error = false;
                             break;
                         }
                         default:
@@ -223,5 +335,14 @@ public class BelCoopSoyuzSQLHandler extends DefaultCashRegisterHandler<BelCoopSo
 
     protected String trim(String input, Integer length) {
         return input == null ? null : (length == null || length >= input.trim().length() ? input.trim() : input.trim().substring(0, length));
+    }
+
+    private String appendSpaces(String line, int length) {
+        if(line == null)
+            line = "";
+        StringBuilder lineBuilder = new StringBuilder(line.substring(0, Math.min(line.length(), length)));
+        while(lineBuilder.length() < length)
+            lineBuilder.append(" ");
+        return lineBuilder.toString();
     }
 }
