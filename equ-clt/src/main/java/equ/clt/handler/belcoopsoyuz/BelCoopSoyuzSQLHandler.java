4e99271c63ff7cd542095faf0da315c51a7d47c0
AFilipchyk <filipchyk.andrei@gmail.com>
2018-11-28 11:04:01 +0000

    [equ] BelCoopSoyuzSQL: CECUCOD

--- equ-clt/src/main/java/equ/clt/handler/belcoopsoyuz/BelCoopSoyuzSQLHandler.java
+++ equ-clt/src/main/java/equ/clt/handler/belcoopsoyuz/BelCoopSoyuzSQLHandler.java
@@ -92,13 +92,13 @@ public class BelCoopSoyuzSQLHandler extends DefaultCashRegisterHandler<BelCoopSo
                 }
                 conn.setAutoCommit(false);
                 ps = conn.prepareStatement("MERGE INTO cl1_bks.l9sk34 dest " +
-                                "USING(SELECT ? CEUNIKEY, ? CEUNIREF0, ? CEDOCCOD, ? CEOBIDE, ? CEOBMEA, ? MEOBNAM, ? NERECOST, ? NEOPPRIC, ? TEDOCACT FROM DUAL) src " +
+                                "USING(SELECT ? CEUNIKEY, ? CEUNIREF0, ? CEDOCCOD, ? CEOBIDE, ? CEOBMEA, ? MEOBNAM, ? NERECOST, ? NEOPPRIC, ? TEDOCACT, ? CECUCOD FROM DUAL) src " +
                                 "ON (dest.CEUNIKEY = src.CEUNIKEY) " +
                                 "WHEN MATCHED THEN " +
-                                "UPDATE SET dest.CEUNIREF0=src.CEUNIREF0, dest.CEDOCCOD=src.CEDOCCOD, dest.CEOBIDE=src.CEOBIDE, dest.CEOBMEA=src.CEOBMEA, dest.MEOBNAM=src.MEOBNAM, dest.NERECOST=src.NERECOST, dest.NEOPPRIC=src.NEOPPRIC, dest.TEDOCACT=src.TEDOCACT " +
+                                "UPDATE SET dest.CEUNIREF0=src.CEUNIREF0, dest.CEDOCCOD=src.CEDOCCOD, dest.CEOBIDE=src.CEOBIDE, dest.CEOBMEA=src.CEOBMEA, dest.MEOBNAM=src.MEOBNAM, dest.NERECOST=src.NERECOST, dest.NEOPPRIC=src.NEOPPRIC, dest.TEDOCACT=src.TEDOCACT, dest.CECUCOD=src.CECUCOD " +
                                 "WHEN NOT MATCHED THEN " +
-                                "INSERT (CEUNIKEY, CEUNIREF0, CEDOCCOD, CEOBIDE, CEOBMEA, MEOBNAM, NERECOST, NEOPPRIC, TEDOCACT) " +
-                                "VALUES (src.CEUNIKEY, src.CEUNIREF0, src.CEDOCCOD, src.CEOBIDE, src.CEOBMEA, src.MEOBNAM, src.NERECOST, src.NEOPPRIC, src.TEDOCACT)"
+                                "INSERT (CEUNIKEY, CEUNIREF0, CEDOCCOD, CEOBIDE, CEOBMEA, MEOBNAM, NERECOST, NEOPPRIC, TEDOCACT, CECUCOD) " +
+                                "VALUES (src.CEUNIKEY, src.CEUNIREF0, src.CEDOCCOD, src.CEOBIDE, src.CEOBMEA, src.MEOBNAM, src.NERECOST, src.NEOPPRIC, src.TEDOCACT, src.CECUCOD)"
                 );
 
                 for (CashRegisterItemInfo item : transaction.itemsList) {
@@ -113,6 +113,7 @@ public class BelCoopSoyuzSQLHandler extends DefaultCashRegisterHandler<BelCoopSo
                     ps.setDouble(7, item.price.doubleValue()); //NERECOST, роз.цена
                     ps.setDouble(8, item.price.doubleValue()); //NEOPPRIC, роз.цена
                     ps.setTimestamp(9, new java.sql.Timestamp(transaction.date.getTime())); //TEDOCACT, дата и время вступления прайса в силу
+                    ps.setString(10, item.section); //CECUCOD, секция
                     ps.addBatch();
                 }
 
