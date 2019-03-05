1309d60a55c027258b9ea43299515ed8fcb9c397
AFilipchyk <filipchyk.andrei@gmail.com>
2019-03-05 09:05:01 +0000

    Revert: [equ-srv] DefaultTerminalHandler: catch LSFException

--- equ-srv/src/main/java/equ/srv/terminal/DefaultTerminalHandler.java
+++ equ-srv/src/main/java/equ/srv/terminal/DefaultTerminalHandler.java
@@ -18,7 +18,6 @@ import lsfusion.server.data.expr.KeyExpr;
 import lsfusion.server.data.query.QueryBuilder;
 import lsfusion.server.integration.*;
 import lsfusion.server.logics.*;
-import lsfusion.server.logics.property.actions.LSFException;
 import lsfusion.server.logics.scripted.ScriptingErrorLog;
 import lsfusion.server.logics.scripted.ScriptingLogicsModule;
 import lsfusion.server.session.DataSession;
@@ -752,13 +751,7 @@ public class DefaultTerminalHandler implements TerminalHandlerInterface {
                 }
                 terminalHandlerLM.findAction("process[TerminalDocument]").execute(session, stack, terminalDocumentObject);
                 ServerLoggers.importLogger.info("start applying terminal document " + idTerminalDocument);
-
-                String result;
-                try {
-                    result = session.applyMessage(getLogicsInstance().getBusinessLogics(), stack);
-                } catch (LSFException e) {
-                    result = e.getMessage();
-                }
+                String result = session.applyMessage(getLogicsInstance().getBusinessLogics(), stack);
                 if(result != null) {
                     ServerLoggers.importLogger.error(String.format("Apply terminal document %s error: %s", idTerminalDocument, result));
                 }
