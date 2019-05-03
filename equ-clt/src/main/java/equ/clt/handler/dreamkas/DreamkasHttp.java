aac0464e807ecb656d5cf9bf38fc1f8c26dfe4ee
Eveigh <varets.p@gmail.com>
2019-05-03 09:27:41 +0000

    [equ-clt] dreamkas: исправлена кодировка в ответе

--- equ-clt/src/main/java/equ/clt/handler/dreamkas/DreamkasHttp.java
+++ equ-clt/src/main/java/equ/clt/handler/dreamkas/DreamkasHttp.java
@@ -10,6 +10,7 @@ import org.apache.http.impl.client.HttpClients;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 // Класс обработчика выполнения HTTP(S) запросов
@@ -113,7 +114,7 @@ public class DreamkasHttp {
             cStatus = response.getStatusLine().toString();
             //  читаем ответ
             if (!cStatus.toUpperCase().contains("NO CONTENT")) {
-                BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+                BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), StandardCharsets.UTF_8));
                 StringBuilder result = new StringBuilder();
                 String line;
                 while ((line = rd.readLine()) != null) {
