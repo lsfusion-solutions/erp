c6a627b22c59d41ea97f7a3c35bfee7085da6992
Eveigh <varets.p@gmail.com>
2019-11-12 07:43:10 +0000

    [equ-clt] astron: принимаем код дисконтной карты а не клиента

--- equ-clt/src/main/java/equ/clt/handler/astron/AstronHandler.java
+++ equ-clt/src/main/java/equ/clt/handler/astron/AstronHandler.java
@@ -1029,7 +1029,7 @@ public class AstronHandler extends DefaultCashRegisterHandler<AstronSalesBatch> 
 
         try (Statement statement = conn.createStatement()) {
             String query = "SELECT sales.SALESATTRS, sales.SYSTEMID, sales.SESSID, sales.SALESTIME, sales.FRECNUM, sales.SRECNUM, sales.CASHIERID, cashier.CASHIERNAME, " +
-                    "sales.SALESTAG, sales.SALESBARC, sales.SALESCODE, sales.SALESCOUNT, sales.SALESPRICE, sales.SALESSUM, sales.SALESDISC, sales.SALESTYPE, sales.CLNTID, " +
+                    "sales.SALESTAG, sales.SALESBARC, sales.SALESCODE, sales.SALESCOUNT, sales.SALESPRICE, sales.SALESSUM, sales.SALESDISC, sales.SALESTYPE, " +
                     "sales." + getSalesNumField() + ", sales.SAREAID, sales." + getSalesRefundField() + ", COALESCE(sess.SESSSTART,sales.SALESTIME) AS SESSSTART FROM SALES sales " +
                     "LEFT JOIN (SELECT SESSID, SYSTEMID, SAREAID, max(SESSSTART) AS SESSSTART FROM SESS GROUP BY SESSID, SYSTEMID, SAREAID) sess " +
                     "ON sales.SESSID=sess.SESSID AND sales.SYSTEMID=sess.SYSTEMID AND sales.SAREAID=sess.SAREAID " +
@@ -1149,7 +1149,7 @@ public class AstronHandler extends DefaultCashRegisterHandler<AstronSalesBatch> 
                         curSalesInfoList = new ArrayList<>();
                         curRecordList = new ArrayList<>();
                         prologSum = rs.getBigDecimal("SALESSUM");
-                        idDiscountCard = rs.getString("CLNTID");
+                        idDiscountCard = rs.getString("SALESBARC");
 
                         if (isReturn) { //чек возврата
                             String salesAttrs = rs.getString("SALESATTRS");
