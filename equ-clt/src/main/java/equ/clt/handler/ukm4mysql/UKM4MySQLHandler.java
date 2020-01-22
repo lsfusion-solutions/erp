ff31e928c8dbc29d3c185a68fe3640c03edc8885
AFilipchyk <filipchyk.andrei@gmail.com>
2020-01-22 09:10:11 +0000

    [equ] UKM4MySQL: startDate

--- equ-clt/src/main/java/equ/clt/handler/ukm4mysql/UKM4MySQLHandler.java
+++ equ-clt/src/main/java/equ/clt/handler/ukm4mysql/UKM4MySQLHandler.java
@@ -1217,11 +1217,13 @@ public class UKM4MySQLHandler extends DefaultCashRegisterHandler<UKM4MySQLSalesB
                     totalQuantity = isSale ? totalQuantity : isReturn ? totalQuantity.negate() : null;
                     BigDecimal discountSumReceiptDetail = HandlerUtils.safeSubtract(sum, realAmount);
                     if (totalQuantity != null) {
-                        salesInfoList.add(getSalesInfo(isGiftCard, false, nppGroupMachinery, cash_id, numberZReport,
-                                dateZReport, timeZReport, numberReceipt, dateReceipt, timeReceipt, idEmployee,
-                                null, lastNameContact, paymentEntry.sumCard, paymentEntry.sumCash, sumGiftCardMap, idBarcode, idItem, null, null, totalQuantity,
-                                price, isSale ? realAmount : realAmount.negate(), null, discountSumReceiptDetail, null, discountCard,
-                                position, null, idSection, false, cashRegister));
+                        if (cashRegister.startDate == null || (dateReceipt != null && dateReceipt.compareTo(cashRegister.startDate) >= 0)) {
+                            salesInfoList.add(getSalesInfo(isGiftCard, false, nppGroupMachinery, cash_id, numberZReport,
+                                    dateZReport, timeZReport, numberReceipt, dateReceipt, timeReceipt, idEmployee,
+                                    null, lastNameContact, paymentEntry.sumCard, paymentEntry.sumCash, sumGiftCardMap, idBarcode, idItem, null, null, totalQuantity,
+                                    price, isSale ? realAmount : realAmount.negate(), null, discountSumReceiptDetail, null, discountCard,
+                                    position, null, idSection, false, cashRegister));
+                        }
                         receiptSet.add(Pair.create(idReceipt, cash_id));
                     }
                 }
