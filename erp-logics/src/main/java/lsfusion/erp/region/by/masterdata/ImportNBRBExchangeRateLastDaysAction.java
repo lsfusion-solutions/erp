6b7e0415f007e67117cf58d85becad5e83cdaf16
AFilipchyk <filipchyk.andrei@gmail.com>
2020-03-17 13:10:40 +0000

    [erp] param check for import nbrb

--- erp-logics/src/main/java/lsfusion/erp/region/by/masterdata/ImportNBRBExchangeRateLastDaysAction.java
+++ erp-logics/src/main/java/lsfusion/erp/region/by/masterdata/ImportNBRBExchangeRateLastDaysAction.java
@@ -37,18 +37,21 @@ public class ImportNBRBExchangeRateLastDaysAction extends ImportNBRBExchangeRate
             String shortNameCurrency = (String) findProperty("shortName[Currency]").read(context, currencyObject);
             long currentTime = Calendar.getInstance().getTimeInMillis();
             Integer days = (Integer) findProperty("importNBRBExchangeRateDaysCount[]").read(context);
-            if (shortNameCurrency != null && days != null && days > 0) {
-                Date separationDate = new Date(2016 - 1900, 5, 30); //30.06.2016
-                Date denominationDate = new Date(2016 - 1900, 6, 1); //01.07.2016
-                Date dateFrom = new Date(currentTime - (long) days * 24 * 3600 * 1000);
-                Date dateTo = new Date(currentTime + (long) 24 * 3600 * 1000);
-                if(dateFrom.getTime() <= separationDate.getTime() && dateTo.getTime() > separationDate.getTime()) {
-                    importExchanges(dateFrom, separationDate, shortNameCurrency, context, true);
-                    importExchanges(denominationDate, dateTo, shortNameCurrency, context, false);
-                } else {
-                    importExchanges(dateFrom, dateTo, shortNameCurrency, context, dateFrom.getTime() < separationDate.getTime());
+            if(days != null && days > 0) {
+                if (shortNameCurrency != null) {
+                    Date separationDate = new Date(2016 - 1900, 5, 30); //30.06.2016
+                    Date denominationDate = new Date(2016 - 1900, 6, 1); //01.07.2016
+                    Date dateFrom = new Date(currentTime - (long) days * 24 * 3600 * 1000);
+                    Date dateTo = new Date(currentTime + (long) 24 * 3600 * 1000);
+                    if (dateFrom.getTime() <= separationDate.getTime() && dateTo.getTime() > separationDate.getTime()) {
+                        importExchanges(dateFrom, separationDate, shortNameCurrency, context, true);
+                        importExchanges(denominationDate, dateTo, shortNameCurrency, context, false);
+                    } else {
+                        importExchanges(dateFrom, dateTo, shortNameCurrency, context, dateFrom.getTime() < separationDate.getTime());
+                    }
                 }
-
+            } else {
+                throw new RuntimeException("Параметр 'Кол-во дней' должен быть задан и больше 0");
             }
 
         } catch (IOException | ScriptingErrorLog.SemanticErrorException | ParseException | JSONException e) {
