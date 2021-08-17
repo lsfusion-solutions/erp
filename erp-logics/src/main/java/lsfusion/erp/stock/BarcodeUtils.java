7748240468e9f15323ebdde42b0d15dbe2f140f1
AFilipchyk <filipchyk.andrei@gmail.com>
2021-08-17 11:43:04 +0000

    [erp] Универсальный импорт BarcodeUtils: upc fix

--- erp-logics/src/main/java/lsfusion/erp/stock/BarcodeUtils.java
+++ erp-logics/src/main/java/lsfusion/erp/stock/BarcodeUtils.java
@@ -49,6 +49,30 @@ public class BarcodeUtils {
         }
     }
 
+    public static String appendCheckDigitToBarcodeWithoutUPC(String barcode) {
+        if(barcode == null)
+            return null;
+
+        try {
+            if (barcode.length() == 12) { //EAN-13
+                return appendEAN13(barcode);
+            } else if (barcode.length() == 7) {  //EAN-8
+                int checkSum = 0;
+                for (int i = 0; i <= 6; i = i + 2) {
+                    checkSum += Integer.parseInt(String.valueOf(barcode.charAt(i))) * 3;
+                    checkSum += i == 6 ? 0 : Integer.parseInt(String.valueOf(barcode.charAt(i + 1)));
+                }
+                checkSum %= 10;
+                if (checkSum != 0)
+                    checkSum = 10 - checkSum;
+                return barcode.concat(String.valueOf(checkSum));
+            } else
+                return barcode;
+        } catch (Exception e) {
+            return barcode;
+        }
+    }
+
     private static String appendEAN13(String barcode) {
         int checkSum = 0;
         for (int i = 0; i <= 10; i = i + 2) {
@@ -63,7 +87,7 @@ public class BarcodeUtils {
 
     public static boolean isCheckDigitCorrect(String barcode) {
         if (barcode != null && (barcode.length() == 13 || barcode.length() == 8)) {
-            return barcode.equals(appendCheckDigitToBarcode(barcode.substring(0, barcode.length() - 1)));
+            return barcode.equals(appendCheckDigitToBarcodeWithoutUPC(barcode.substring(0, barcode.length() - 1)));
         }
         return true;
     }
