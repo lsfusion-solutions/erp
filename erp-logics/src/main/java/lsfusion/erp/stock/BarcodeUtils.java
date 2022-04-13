09057597d75facc69610f2301f469b4e69b0aaf9
CrushBy <crushby@gmail.com>
2022-04-13 14:43:09 +0000

    Добавлена возможность печатать GS1 DataMatrix через OkapiBarcode

--- erp-logics/src/main/java/lsfusion/erp/stock/BarcodeUtils.java
+++ erp-logics/src/main/java/lsfusion/erp/stock/BarcodeUtils.java
@@ -1,5 +1,12 @@
 package lsfusion.erp.stock;
 
+import uk.org.okapibarcode.backend.DataMatrix;
+import uk.org.okapibarcode.backend.Symbol;
+import uk.org.okapibarcode.output.Java2DRenderer;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 public class BarcodeUtils {
 
     public static String appendCheckDigitToBarcode(String barcode) {
@@ -90,6 +97,31 @@ public class BarcodeUtils {
             return barcode.equals(appendCheckDigitToBarcodeWithoutUPC(barcode.substring(0, barcode.length() - 1)));
         }
         return true;
+    }
+
+    // sample
+    //lsfusion.erp.stock.BarcodeUtils.generateDataMatrix($F{code(l)},uk.org.okapibarcode.backend.Symbol.DataType.GS1,10)
+    // value should be :
+    //[01]04810140842325[21]swsVofj0irNCT[91]EE06[92]WriL160vD8L7UaOcw4zNUw91zE/FVKM1owyF/EiGx7Y=
+
+    public static BufferedImage generateDataMatrix(String value, Symbol.DataType dataType, int magnification) {
+        DataMatrix barcode = new DataMatrix();
+
+        // We need a GS1 DataMatrix barcode.
+        barcode.setDataType(dataType);
+        // 0 means size will be set automatically according to amount of data (smallest possible).
+        barcode.setPreferredSize(0);
+        // Don't want no funky rectangle shapes, if we can avoid it.
+        barcode.setForceMode(DataMatrix.ForceMode.SQUARE);
+
+        barcode.setContent(value);
+
+        BufferedImage image = new BufferedImage(barcode.getWidth() * magnification, barcode.getHeight() * magnification, BufferedImage.TYPE_BYTE_GRAY);
+        Graphics2D g2d = image.createGraphics();
+        Java2DRenderer renderer = new Java2DRenderer(g2d, magnification, Color.WHITE, Color.BLACK);
+        renderer.render(barcode);
+
+        return image;
     }
 
 }
