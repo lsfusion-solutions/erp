245057df7c6c0ef8ffe3e02683c40767f40ba21e
AFilipchyk <filipchyk.andrei@gmail.com>
2022-06-22 11:42:44 +0000

    [equ-srv] CashDrawer module instead of CashOperation

--- equ-srv/src/main/java/equ/srv/SendSalesEquipmentServer.java
+++ equ-srv/src/main/java/equ/srv/SendSalesEquipmentServer.java
@@ -44,7 +44,10 @@ public class SendSalesEquipmentServer {
 
     public static void init(BusinessLogics BL) {
         cashRegisterLM = BL.getModule("EquipmentCashRegister");
-        cashOperationLM = BL.getModule("CashOperation");
+        cashOperationLM = BL.getModule("CashDrawer");
+        if(cashOperationLM == null) {
+            BL.getModule("CashOperation");
+        }
         equipmentCashRegisterLM = BL.getModule("EquipmentCashRegister");
         machineryPriceTransactionLM = BL.getModule("MachineryPriceTransaction");
         zReportLM = BL.getModule("ZReport");
