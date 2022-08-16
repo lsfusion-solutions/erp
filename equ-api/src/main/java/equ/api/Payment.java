3244a9424c5eaf500ee473e6685225a6151546df
AFilipchyk <filipchyk.andrei@gmail.com>
2022-08-16 12:02:09 +0000

    [equ-api] readSales payment refactoring (equ-api changed!)

---
+++ equ-api/src/main/java/equ/api/Payment.java
@@ -0,0 +1,52 @@
+package equ.api;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Payment implements Serializable {
+    public String type;
+    public BigDecimal sum;
+    public Map<String, Object> extraFields;
+
+    public Payment(Integer type, BigDecimal sum) {
+        this(String.valueOf(type), sum);
+    }
+
+    public Payment(String type, BigDecimal sum) {
+        this(type, sum, null);
+    }
+
+    public Payment(String type, BigDecimal sum, Map<String, Object> extraFields) {
+        this.type = type;
+        this.sum = sum;
+        this.extraFields = extraFields;
+    }
+
+    public static Payment getCash(BigDecimal sum) {
+        return getCash(sum, null);
+    }
+
+    public static Payment getCash(BigDecimal sum, Map<String, Object> extraFields) {
+        return new Payment("cash", sum, extraFields);
+    }
+
+    public static Payment getCard(BigDecimal sum) {
+        return getCard(sum, null);
+    }
+
+    public static Payment getCard(BigDecimal sum, String extraFieldKey, Object extraFieldValue) {
+        Map<String, Object> extraFields = new HashMap<>();
+        extraFields.put(extraFieldKey, extraFieldValue);
+        return getCard(sum, extraFields);
+    }
+
+    public static Payment getCard(BigDecimal sum, Map<String, Object> extraFields) {
+        return new Payment("card", sum, extraFields);
+    }
+
+    public boolean isCard() {
+        return type.equals("card");
+    }
+}
