164876c8f33f35f07fa08cead99165e1ef541881
AFilipchyk <filipchyk.andrei@gmail.com>
2025-02-24 08:08:18 +0000

    [equ] UKM extra logs

--- equ-clt/src/main/java/equ/clt/handler/ukm4mysql/UKM4MySQLHandler.java
+++ equ-clt/src/main/java/equ/clt/handler/ukm4mysql/UKM4MySQLHandler.java
@@ -210,6 +210,7 @@ public class UKM4MySQLHandler extends DefaultCashRegisterHandler<UKM4MySQLSalesB
                                     versionTransactionMap.put(version, transaction.id);
 
                                 } catch (Exception e) {
+                                    processTransactionLogger.error(logPrefix + "process transaction failed", e);
                                     exception = e;
                                 }
                                 sendTransactionBatchMap.put(transaction.id, new SendTransactionBatch(exception));
@@ -748,7 +749,7 @@ public class UKM4MySQLHandler extends DefaultCashRegisterHandler<UKM4MySQLSalesB
     }
 
     private JSONObject getExtInfo(String extInfo) {
-        return extInfo != null && !extInfo.isEmpty() ? new JSONObject(extInfo).optJSONObject("ukm") : null;
+        return getExtInfo(extInfo, "ukm");
     }
 
     private void exportSignals(Connection conn, TransactionCashRegisterInfo transaction, int version, boolean ignoreSnapshot, int timeout, boolean wait) throws SQLException {
