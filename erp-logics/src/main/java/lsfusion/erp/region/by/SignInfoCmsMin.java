584cb8d814a59cf479df8f9cd41cea1d1f1b3d85
rebkovetsa <email@example.org>
2025-06-19 14:02:29 +0000

    Преобразование подписи для СККО

--- erp-logics/src/main/java/lsfusion/erp/region/by/SignInfoCmsMin.java
+++ erp-logics/src/main/java/lsfusion/erp/region/by/SignInfoCmsMin.java
@@ -1,26 +1,28 @@
 package lsfusion.erp.region.by;
 
 import com.google.common.base.Throwables;
+import org.bouncycastle.asn1.x509.Extension;
+import org.bouncycastle.asn1.x509.SubjectKeyIdentifier;
+import org.bouncycastle.cert.X509CertificateHolder;
 import org.bouncycastle.cms.CMSSignedData;
 import org.bouncycastle.cms.SignerInformation;
 import org.bouncycastle.asn1.*;
-import org.bouncycastle.asn1.pkcs.PKCSObjectIdentifiers;
-import org.bouncycastle.asn1.cms.Attribute;
-import org.bouncycastle.asn1.cms.AttributeTable;
+import org.bouncycastle.util.Store;
 import org.bouncycastle.util.encoders.Base64;
 
-import lsfusion.server.logics.UtilsLogicsModule;
+import lsfusion.server.language.ScriptingLogicsModule;
 import lsfusion.server.logics.action.controller.context.ExecutionContext;
 import lsfusion.server.logics.classes.ValueClass;
 import lsfusion.server.logics.property.classes.ClassPropertyInterface;
 import lsfusion.server.physics.dev.integration.internal.to.InternalAction;
 
+import java.util.Collection;
 import java.util.Iterator;
 
 public class SignInfoCmsMin extends InternalAction {
     private final ClassPropertyInterface fileInterface;
 
-    public SignInfoCmsMin(UtilsLogicsModule LM, ValueClass... classes) {
+    public SignInfoCmsMin(ScriptingLogicsModule LM, ValueClass... classes) {
         super(LM, classes);
 
         Iterator<ClassPropertyInterface> i = getOrderInterfaces().iterator();
@@ -39,17 +41,27 @@ public class SignInfoCmsMin extends InternalAction {
 
             // 2. Берём первого подписанта
             SignerInformation signer = signedData.getSignerInfos().getSigners().iterator().next();
-            // 3. Извлекаем messageDigest из SignedAttributes
-            AttributeTable attrs = signer.getSignedAttributes();
-            Attribute digestAttr = attrs.get(PKCSObjectIdentifiers.pkcs_9_at_messageDigest);
-            ASN1OctetString digest = (ASN1OctetString) digestAttr.getAttrValues().getObjectAt(0);
-            byte[] hashBytes = digest.getOctets();
+            // 3. Извлекаем SubjectKeyIdentifier
+            Store<X509CertificateHolder> certStore = signedData.getCertificates();
+            Collection<X509CertificateHolder> certs = certStore.getMatches(null);
+            
+            byte[] keyId = new byte[0];
+            for (X509CertificateHolder holder : certs) {
+                Extension ext = holder.getExtension(Extension.subjectKeyIdentifier);
+
+                if (ext != null) {
+                    keyId = SubjectKeyIdentifier
+                            .getInstance(ASN1OctetString.getInstance(ext.getExtnValue().getOctets()).getOctets())
+                            .getKeyIdentifier();
+                }
+            }
+
             // 4. Подпись
             byte[] signatureBytes = signer.getSignature();
             // 5. Собираем signInfo вручную
             ASN1EncodableVector v = new ASN1EncodableVector();
             v.add(new ASN1Integer(3));
-            v.add(new DERTaggedObject(false, 0, new DEROctetString(hashBytes)));
+            v.add(new DERTaggedObject(false, 0, new DEROctetString(keyId)));
             v.add(new DERSequence(new ASN1Encodable[] {
                     new ASN1ObjectIdentifier("1.2.112.0.2.0.34.101.31.81"), DERNull.INSTANCE }));
             v.add(new DERSequence(new ASN1Encodable[] {
