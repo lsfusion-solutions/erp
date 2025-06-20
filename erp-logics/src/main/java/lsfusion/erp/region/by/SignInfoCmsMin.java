086dbf19def3c1f283304f7396f2c57eebdbf04f
Eveigh <varets.p@gmail.com>
2025-06-20 14:17:47 +0000

    Подписание для СККО

--- erp-logics/src/main/java/lsfusion/erp/region/by/SignInfoCmsMin.java
+++ erp-logics/src/main/java/lsfusion/erp/region/by/SignInfoCmsMin.java
@@ -1,13 +1,10 @@
 package lsfusion.erp.region.by;
 
+import by.avest.crypto.pkcs11.provider.ProviderFactory;
 import com.google.common.base.Throwables;
+import lsfusion.base.SystemUtils;
 import org.bouncycastle.asn1.x509.Extension;
-import org.bouncycastle.asn1.x509.SubjectKeyIdentifier;
-import org.bouncycastle.cert.X509CertificateHolder;
-import org.bouncycastle.cms.CMSSignedData;
-import org.bouncycastle.cms.SignerInformation;
 import org.bouncycastle.asn1.*;
-import org.bouncycastle.util.Store;
 import org.bouncycastle.util.encoders.Base64;
 
 import lsfusion.server.language.ScriptingLogicsModule;
@@ -16,64 +13,105 @@ import lsfusion.server.logics.classes.ValueClass;
 import lsfusion.server.logics.property.classes.ClassPropertyInterface;
 import lsfusion.server.physics.dev.integration.internal.to.InternalAction;
 
-import java.util.Collection;
-import java.util.Iterator;
+import java.io.IOException;
+import java.security.cert.X509Certificate;
+import java.util.*;
+import java.security.*;
 
 public class SignInfoCmsMin extends InternalAction {
     private final ClassPropertyInterface fileInterface;
+    private final ClassPropertyInterface aliasInterface;
+    private final ClassPropertyInterface passwordInterface;
+    private final ClassPropertyInterface pathInterface;
+
 
     public SignInfoCmsMin(ScriptingLogicsModule LM, ValueClass... classes) {
         super(LM, classes);
 
         Iterator<ClassPropertyInterface> i = getOrderInterfaces().iterator();
         fileInterface = i.next();
+        aliasInterface = i.next();
+        passwordInterface = i.next();
+        pathInterface = i.next();
     }
 
     @Override
     protected void executeInternal(ExecutionContext<ClassPropertyInterface> context) {
         try {
-            String cmsBase64 = (String) context.getDataKeyValue(fileInterface).getValue();
-            // 1. Декодируем CMS
-            byte[] cmsBytes = Base64.decode(cmsBase64);
-            CMSSignedData signedData = null;
+            String dataBase64 = (String) context.getDataKeyValue(fileInterface).getValue();
+            byte[] dataBytes = Base64.decode(dataBase64);
 
-            signedData = new CMSSignedData(cmsBytes);
+            String alias = (String) context.getDataKeyValue(aliasInterface).getValue();
+            String password = (String) context.getDataKeyValue(passwordInterface).getValue();
+            String path = (String) context.getDataKeyValue(pathInterface).getValue();
 
-            // 2. Берём первого подписанта
-            SignerInformation signer = signedData.getSignerInfos().getSigners().iterator().next();
-            // 3. Извлекаем SubjectKeyIdentifier
-            Store<X509CertificateHolder> certStore = signedData.getCertificates();
-            Collection<X509CertificateHolder> certs = certStore.getMatches(null);
-            
-            byte[] keyId = new byte[0];
-            for (X509CertificateHolder holder : certs) {
-                Extension ext = holder.getExtension(Extension.subjectKeyIdentifier);
 
-                if (ext != null) {
-                    keyId = SubjectKeyIdentifier
-                            .getInstance(ASN1OctetString.getInstance(ext.getExtnValue().getOctets()).getOctets())
-                            .getKeyIdentifier();
-                }
-            }
+            // Подписываем, получаем CMS
+            byte[] cmsBytes = signData(dataBytes, alias, password.toCharArray(), path);
 
-            // 4. Подпись
-            byte[] signatureBytes = signer.getSignature();
-            // 5. Собираем signInfo вручную
-            ASN1EncodableVector v = new ASN1EncodableVector();
-            v.add(new ASN1Integer(3));
-            v.add(new DERTaggedObject(false, 0, new DEROctetString(keyId)));
-            v.add(new DERSequence(new ASN1Encodable[] {
-                    new ASN1ObjectIdentifier("1.2.112.0.2.0.34.101.31.81"), DERNull.INSTANCE }));
-            v.add(new DERSequence(new ASN1Encodable[] {
-                    new ASN1ObjectIdentifier("1.2.112.0.2.0.34.101.45.2.1"), DERNull.INSTANCE }));
-            v.add(new DEROctetString(signatureBytes));
-            DERSequence signInfo = new DERSequence(v);
-            byte[] signInfoBytes = signInfo.getEncoded("DER");
-            // 6. Возвращаем Base64
-            findProperty("rusultSignInfo[]").change(Base64.toBase64String(signInfoBytes), context);
+            // Возвращаем Base64
+            findProperty("rusultSignInfo[]").change(Base64.toBase64String(cmsBytes), context);
         } catch (Exception e) {
             throw Throwables.propagate(e);
         }
 
+    }
+
+    public byte[] signData(byte[] dataToSign, String keyAlias, char[] pass, String path) throws Exception {
+
+        String libraryPath = System.getProperty("java.library.path");
+        String libPath = path + (SystemUtils.is64Arch() ? "/win64" : "/win32");
+        if (libraryPath != null && !libraryPath.contains(libPath))
+            System.setProperty("java.library.path", libPath + (libraryPath.isEmpty() ? "" : (";" + libraryPath)));
+        System.setProperty("by.avest.loader.shared", "true");
+
+        ProviderFactory.addAvUniversalProvider();
+
+        KeyStore store = KeyStore.getInstance("AvPersonal");
+        store.load(null, null);
+
+        Enumeration<String> aliases = store.aliases();
+        while (aliases.hasMoreElements()) {
+            String alias = aliases.nextElement();
+            if (alias.equals(keyAlias)) {
+                PrivateKey key = (PrivateKey) store.getKey(alias, pass);
+
+                Signature signature = Signature.getInstance("BELTWITHBIGN", "AvUniversal");
+                signature.initSign(key);
+                signature.update(dataToSign);
+                byte[] signedData = signature.sign();
+
+                if (signedData != null){
+                    X509Certificate cert = (X509Certificate) store.getCertificate(alias);
+                    byte[] extValue = cert.getExtensionValue(Extension.subjectKeyIdentifier.getId());
+                    if (extValue != null) {
+                        byte[] skiBytes = ASN1OctetString.getInstance(ASN1OctetString.getInstance(extValue).getOctets()).getOctets();
+                        return buildCustomCMS(signedData, skiBytes);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public byte[] buildCustomCMS(byte[] signedData, byte[] skiBytes) throws IOException {
+        ASN1EncodableVector cmsVector = new ASN1EncodableVector();
+
+        cmsVector.add(new ASN1Integer(3)); // Версия
+        cmsVector.add(new DERTaggedObject(false, 0, new DEROctetString(skiBytes))); // SKI
+
+        cmsVector.add(new DERSequence(new ASN1Encodable[] {
+                new ASN1ObjectIdentifier("1.2.112.0.2.0.34.101.31.81"),
+                DERNull.INSTANCE
+        })); // Hash алгоритм
+
+        cmsVector.add(new DERSequence(new ASN1Encodable[] {
+                new ASN1ObjectIdentifier("1.2.112.0.2.0.34.101.45.2.1"),
+                DERNull.INSTANCE
+        })); // Алгоритм подписи
+
+        cmsVector.add(new DEROctetString(signedData)); // Подпись
+
+        return new DERSequence(cmsVector).getEncoded();
     }
 }
