22cac9e0b064123d8592e219940306090aabd479
AFilipchyk <filipchyk.andrei@gmail.com>
2025-06-23 06:55:22 +0000

    [erp] SingAvestClientAction

---
+++ erp-logics/src/main/java/lsfusion/erp/region/by/SignAvestClientAction.java
@@ -0,0 +1,94 @@
+package lsfusion.erp.region.by;
+
+import by.avest.crypto.pkcs11.provider.ProviderFactory;
+import lsfusion.base.SystemUtils;
+import lsfusion.interop.action.ClientAction;
+import lsfusion.interop.action.ClientActionDispatcher;
+import org.bouncycastle.asn1.*;
+import org.bouncycastle.asn1.x509.Extension;
+
+import java.io.IOException;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.Signature;
+import java.security.cert.X509Certificate;
+import java.util.*;
+
+public class SignAvestClientAction implements ClientAction {
+    byte[] dataToSign;
+    String keyAlias;
+    char[] pass;
+    String path;
+
+    public SignAvestClientAction(byte[] dataToSign, String keyAlias, char[] pass, String path) {
+        this.dataToSign = dataToSign;
+        this.keyAlias = keyAlias;
+        this.pass = pass;
+        this.path = path;
+    }
+
+    public Object dispatch(ClientActionDispatcher dispatcher) throws IOException {
+        try {
+            return signData(dataToSign, keyAlias, pass, path);
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+
+    public byte[] signData(byte[] dataToSign, String keyAlias, char[] pass, String path) throws Exception {
+        String libraryPath = System.getProperty("java.library.path");
+        String libPath = path + (SystemUtils.is64Arch() ? "/win64" : "/win32");
+        if (libraryPath != null && !libraryPath.contains(libPath))
+            System.setProperty("java.library.path", libPath + (libraryPath.isEmpty() ? "" : (";" + libraryPath)));
+        System.setProperty("by.avest.loader.shared", "true");
+
+        ProviderFactory.addAvUniversalProvider();
+
+        KeyStore store = KeyStore.getInstance("AvPersonal");
+        store.load(null, null);
+
+        Enumeration<String> aliases = store.aliases();
+        while (aliases.hasMoreElements()) {
+            String alias = aliases.nextElement();
+            if (alias.equals(keyAlias)) {
+                PrivateKey key = (PrivateKey) store.getKey(alias, pass);
+
+                Signature signature = Signature.getInstance("BELTWITHBIGN", "AvUniversal");
+                signature.initSign(key);
+                signature.update(dataToSign);
+                byte[] signedData = signature.sign();
+
+                if (signedData != null) {
+                    X509Certificate cert = (X509Certificate) store.getCertificate(alias);
+                    byte[] extValue = cert.getExtensionValue(Extension.subjectKeyIdentifier.getId());
+                    if (extValue != null) {
+                        byte[] skiBytes = ASN1OctetString.getInstance(ASN1OctetString.getInstance(extValue).getOctets()).getOctets();
+                        return buildCustomCMS(signedData, skiBytes);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public byte[] buildCustomCMS(byte[] signedData, byte[] skiBytes) throws IOException {
+        ASN1EncodableVector cmsVector = new ASN1EncodableVector();
+
+        cmsVector.add(new ASN1Integer(3)); // Версия
+        cmsVector.add(new DERTaggedObject(false, 0, new DEROctetString(skiBytes))); // SKI
+
+        cmsVector.add(new DERSequence(new ASN1Encodable[]{
+                new ASN1ObjectIdentifier("1.2.112.0.2.0.34.101.31.81"),
+                DERNull.INSTANCE
+        })); // Hash алгоритм
+
+        cmsVector.add(new DERSequence(new ASN1Encodable[]{
+                new ASN1ObjectIdentifier("1.2.112.0.2.0.34.101.45.2.1"),
+                DERNull.INSTANCE
+        })); // Алгоритм подписи
+
+        cmsVector.add(new DEROctetString(signedData)); // Подпись
+
+        return new DERSequence(cmsVector).getEncoded();
+    }
+}
