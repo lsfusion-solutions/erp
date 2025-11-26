5683f603189a56f133d2304a43389fa85ce16160
AFilipchyk <filipchyk.andrei@gmail.com>
2025-11-26 08:20:04 +0000

    [erp] ScannerDaemon: messageClientAction refactoring

--- erp-logics/src/main/java/lsfusion/erp/daemon/ScannerDaemonAction.java
+++ erp-logics/src/main/java/lsfusion/erp/daemon/ScannerDaemonAction.java
@@ -1,15 +1,19 @@
 package lsfusion.erp.daemon;
 
-import lsfusion.interop.action.MessageClientAction;
+import com.google.common.base.Throwables;
+import lsfusion.server.data.sql.exception.SQLHandledException;
+import lsfusion.server.language.ScriptingErrorLog;
 import lsfusion.server.language.ScriptingLogicsModule;
 import lsfusion.server.logics.action.controller.context.ExecutionContext;
 import lsfusion.server.logics.classes.ValueClass;
 import lsfusion.server.logics.property.classes.ClassPropertyInterface;
 import lsfusion.server.physics.dev.integration.internal.to.InternalAction;
 
+import java.sql.SQLException;
 import java.util.Iterator;
 
 import static lsfusion.base.BaseUtils.trim;
+import static lsfusion.base.BaseUtils.trimToNull;
 
 public class ScannerDaemonAction extends InternalAction {
     private final ClassPropertyInterface comPortInterface;
@@ -27,6 +31,7 @@ public class ScannerDaemonAction extends InternalAction {
 
     @Override
     protected void executeInternal(ExecutionContext<ClassPropertyInterface> context) {
+        String result = null;
         Integer comPort = (Integer) context.getKeyValue(comPortInterface).getValue();
         if(comPort != null) {
             boolean singleRead = context.getKeyValue(singleReadInterface).getValue() != null;
@@ -38,10 +43,12 @@ public class ScannerDaemonAction extends InternalAction {
                 throw new RuntimeException("Pure Java Comm not supported for Scanner Daemon");
             }
 
-            String result = (String) context.requestUserInteraction(new ScannerDaemonClientAction(comPort, singleRead, useJssc));
-            if (result != null && !result.isEmpty()) {
-                context.delayUserInteraction(new MessageClientAction(result, "Ошибка"));
-            }
+            result = (String) context.requestUserInteraction(new ScannerDaemonClientAction(comPort, singleRead, useJssc));
+        }
+        try {
+            findProperty("scannerDaemonError[]").change(trimToNull(result), context);
+        } catch (SQLException | SQLHandledException | ScriptingErrorLog.SemanticErrorException e) {
+            throw Throwables.propagate(e);
         }
     }
 
